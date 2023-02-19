4a3d83f4be5aa69d56e26d8fe6f4301d5a08bd42
hieuck <hieuck@users.noreply.github.com>
2023-02-19 19:58:16 +0700

    Update TeamViewer - Download - Setup - wget.bat

--- TeamViewer - Download - Setup - wget.bat
+++ TeamViewer - Download - Setup - wget.bat
@@ -14,37 +14,45 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat TeamViewer. Vui Long Cho
 @echo off
+
+taskkill /F /IM TeamViewer.exe
 if exist %Windir%\SysWOW64 goto X64
 
-if exist TeamViewer_Setup.exe goto I32
-if not exist TeamViewer_Setup.exe goto D32
+if exist TeamViewer*32*.exe goto I32
+if not exist TeamViewer*32*.exe goto D32
 
 :D32
-wget -q --show-progress https://download.teamviewer.com/download/TeamViewer_Setup.exe
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong TeamViewer...
+wget -O TeamViewer-32_HieuckIT.exe -q --show-progress https://download.teamviewer.com/download/TeamViewer_Setup.exe
+@echo Tai Xuong TeamViewer Hoan Thanh.
 goto I32
 
 :I32
-@echo Dang Cai Dat...
-FOR %%i IN ("Team*Setup.exe") DO Set FileName="%%i"
+@echo Dang Cai Dat TeamViewer...
+FOR %%i IN ("TeamViewer*32*.exe") DO Set FileName="%%i"
 %FileName% /S
-@echo Cai Dat Thanh Cong.
-goto END
+@echo Cai Dat TeamViewer Thanh Cong.
+goto Lic
 
 :X64
-if  exist TeamViewer_Setup_x64.exe goto I64
-if not exist TeamViewer_Setup_x64.exe goto D64
+if  exist TeamViewer*64*.exe goto I64
+if not exist TeamViewer*64*..exe goto D64
 
 :D64
-wget -q --show-progress https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong TeamViewer...
+wget -O TeamViewer-64_HieuckIT.exe -q --show-progress https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe
+@echo Tai Xuong TeamViewer Hoan Thanh.
 goto I64
 
 :I64
-@echo Dang Cai Dat...
-FOR %%i IN ("Team*Setup*64.exe") DO Set FileName="%%i"
+@echo Dang Cai Dat TeamViewer...
+FOR %%i IN ("TeamViewer*64*.exe") DO Set FileName="%%i"
 %FileName% /S
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat TeamViewer Thanh Cong.
+goto Lic
+
+:Lic
+::copy /y "banquyenneuco" "vaoday"
 goto END
 
 :END
