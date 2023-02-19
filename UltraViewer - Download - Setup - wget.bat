72e9270d7bb6029d0779ca28418c84d36ab01ba1
hieuck <hieuck@users.noreply.github.com>
2023-02-19 20:41:55 +0700

    Update UltraViewer - Download - Setup - wget.bat

--- UltraViewer - Download - Setup - wget.bat
+++ UltraViewer - Download - Setup - wget.bat
@@ -14,39 +14,45 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat UltraViewer. Vui Long Cho
 @echo off
+
+taskkill /F /IM UltraViewer_Desktop.exe
 if exist %Windir%\SysWOW64 goto X64
 
-if exist UltraViewer*.exe goto I32
-if not exist UltraViewer*.exe goto D32
+if exist UltraViewer*32*.exe goto I32
+if not exist UltraViewer*32*.exe goto D32
 
 :D32
-@echo Dang Tai Xuong...
-wget -q --show-progress https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong UltraViewer...
+wget -O UltraViewer-32_HieuckIT.exe -q --show-progress https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe
+@echo Tai Xuong UltraViewer Hoan Thanh.
 goto I32
 
 :I32
-@echo Dang Cai Dat...
-FOR %%i IN ("UltraViewer*.exe") DO Set FileName="%%i"
+@echo Dang Cai Dat UltraViewer...
+FOR %%i IN ("UltraViewer*32*.exe") DO Set FileName="%%i"
 %FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-@echo Cai Dat Thanh Cong.
-goto END
+@echo Cai Dat UltraViewer Thanh Cong.
+goto Lic
 
 :X64
-if  exist UltraViewer*.exe goto I64
-if not exist UltraViewer*.exe goto D64
+if  exist UltraViewer*64*.exe goto I64
+if not exist UltraViewer*64*.exe goto D64
 
 :D64
-@echo Dang Tai Xuong...
-wget -q --show-progress https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong UltraViewer...
+wget -O UltraViewer-64_HieuckIT.exe -q --show-progress https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe
+@echo Tai Xuong UltraViewer Hoan Thanh.
 goto I64
 
 :I64
-@echo Dang Cai Dat...
-FOR %%i IN ("UltraViewer*.exe") DO Set FileName="%%i"
+@echo Dang Cai Dat UltraViewer...
+FOR %%i IN ("UltraViewer*64*.exe") DO Set FileName="%%i"
 %FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat UltraViewer Thanh Cong.
+goto Lic
+
+:Lic
+::copy /y "banquyenneuco" "vaoday"
 goto END
 
 :END
