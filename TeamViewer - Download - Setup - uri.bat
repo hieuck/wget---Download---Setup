d81ae63bdae79c09de8e88364ac3f4c34559fcad
hieuck <hieuck@users.noreply.github.com>
2023-02-19 20:33:25 +0700

    Update TeamViewer - Download - Setup - uri.bat

--- TeamViewer - Download - Setup - uri.bat
+++ TeamViewer - Download - Setup - uri.bat
@@ -22,16 +22,16 @@ if exist TeamViewer*32*.exe goto I32
 if not exist TeamViewer*32*.exe goto D32
 
 :D32
-@echo Dang Tai Xuong...
-powershell -Command "Invoke-WebRequest -Uri ''https://download.teamviewer.com/download/TeamViewer_Setup.exe' -OutFile 'TeamViewer_32_HieuckIT.exe'"
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong TeamViewer...
+powershell -Command "Invoke-WebRequest -Uri ''https://download.teamviewer.com/download/TeamViewer_Setup.exe' -OutFile 'TeamViewer-32_HieuckIT.exe'"
+@echo Tai Xuong TeamViewer Hoan Thanh.
 goto I32
 
 :I32
-@echo Dang Cai Dat...
+@echo Dang Cai Dat TeamViewer...
 FOR %%i IN ("TeamViewer*32*.exe") DO Set FileName="%%i"
 %FileName% /S
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat TeamViewer Thanh Cong.
 goto Lic
 
 :X64
@@ -39,16 +39,16 @@ if  exist TeamViewer*64*.exe goto I64
 if not exist TeamViewer*64*..exe goto D64
 
 :D64
-@echo Dang Tai Xuong...
-powershell -Command "Invoke-WebRequest -Uri 'https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe' -OutFile 'TeamViewer_64_HieuckIT.exe'"
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong TeamViewer...
+powershell -Command "Invoke-WebRequest -Uri 'https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe' -OutFile 'TeamViewer-64_HieuckIT.exe'"
+@echo Tai Xuong TeamViewer Hoan Thanh.
 goto I64
 
 :I64
-@echo Dang Cai Dat...
+@echo Dang Cai Dat TeamViewer...
 FOR %%i IN ("TeamViewer*64*.exe") DO Set FileName="%%i"
 %FileName% /S
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat TeamViewer Thanh Cong.
 goto Lic
 
 :Lic
