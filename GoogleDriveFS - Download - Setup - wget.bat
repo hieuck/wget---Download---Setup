d32a12f0336207e235c25cdda66ea03e97334824
hieuck <hieuck@users.noreply.github.com>
2023-02-19 20:48:13 +0700

    Create GoogleDriveFS - Download - Setup - wget.bat

---
+++ GoogleDriveFS - Download - Setup - wget.bat
@@ -0,0 +1,61 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat Google Drive. Vui Long Cho
+@echo off
+
+taskkill /F /IM GoogleDriveFS.exe
+if exist %Windir%\SysWOW64 goto X64
+
+if exist GoogleDriveFS*32*.exe goto I32
+if not exist GoogleDriveFS*32*.exe goto D32
+
+:D32
+@echo Dang Tai Xuong Google Drive...
+wget -O GoogleDriveFS-32_HieuckIT.exe -q --show-progress https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe
+@echo Tai Xuong Google Drive Hoan Thanh.
+goto I32
+
+:I32
+@echo Dang Cai Dat Google Drive...
+FOR %%i IN ("GoogleDriveFS*32*.exe") DO Set FileName="%%i"
+%FileName% /S
+@echo Cai Dat Google Drive Thanh Cong.
+goto Lic
+
+:X64
+if  exist GoogleDriveFS*64*.exe goto I64
+if not exist GoogleDriveFS*64*.exe goto D64
+
+:D64
+@echo Dang Tai Xuong Google Drive...
+wget -O GoogleDriveFS-64_HieuckIT.exe -q --show-progress https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe
+@echo Tai Xuong Google Drive Hoan Thanh.
+goto I64
+
+:I64
+@echo Dang Cai Dat Google Drive...
+FOR %%i IN ("GoogleDriveFS*64*.exe") DO Set FileName="%%i"
+%FileName% /S
+@echo Cai Dat Google Drive Thanh Cong.
+goto Lic
+
+:Lic
+::copy /y "banquyenneuco" "vaoday"
+goto END
+
+:END
+del GoogleDriveFS*.exe
+echo.
+echo Installation completed successfully
