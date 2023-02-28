56fcb59f759863998901b5cef31bbc9486ecef21
hieuck <hieuck@users.noreply.github.com>
2023-02-28 12:52:20 +0700

    Update Foxit PDF Reader - Download - Setup - wget.bat

--- Foxit PDF Reader - Download - Setup - wget.bat
+++ Foxit PDF Reader - Download - Setup - wget.bat
@@ -22,16 +22,16 @@ if exist Foxit*32*.exe goto I32
 if not exist Foxit*32*.exe goto D32
 
 :D32
-@echo Dang Tai Xuong...
+@echo Dang Tai Xuong Foxit Reader...
 wget --no-check-certificate -O Foxit-32_HieuckIT.exe -q --show-progress https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Windows&version=&package_type=&language=English&distID=
-@echo Tai Xuong Hoan Thanh.
+@echo Tai Xuong Foxit Reader Hoan Thanh.
 goto I32
 
 :I32
-@echo Dang Cai Dat...
+@echo Dang Cai Dat Foxit Reader...
 FOR %%i IN ("Foxit*32*.exe") DO Set FileName="%%i"
 %FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat Foxit Reader Thanh Cong.
 goto Lic
 
 :X64
@@ -39,16 +39,16 @@ if  exist Foxit*64*.exe goto I64
 if not exist Foxit*64*..exe goto D64
 
 :D64
-@echo Dang Tai Xuong...
+@echo Dang Tai Xuong Foxit Reader...
 wget --no-check-certificate -O Foxit-64_HieuckIT.exe -q --show-progress https://www.foxit.com/downloads/latest.html?product=Foxit-Reader
-@echo Tai Xuong Hoan Thanh.
+@echo Tai Xuong Foxit Reader Hoan Thanh.
 goto I64
 
 :I64
-@echo Dang Cai Dat...
+@echo Dang Cai Dat Foxit Reader...
 FOR %%i IN ("Foxit*64*.exe") DO Set FileName="%%i"
 %FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat Foxit Reader Thanh Cong.
 goto Lic
 
 :Lic
