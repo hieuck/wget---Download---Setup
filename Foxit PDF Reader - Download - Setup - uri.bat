175bb2d24382a827b8a76f469d48f95fefe042c9
hieuck <hieuck@users.noreply.github.com>
2023-02-28 12:55:35 +0700

    Update Foxit PDF Reader - Download - Setup - uri.bat

--- Foxit PDF Reader - Download - Setup - uri.bat
+++ Foxit PDF Reader - Download - Setup - uri.bat
@@ -15,47 +15,47 @@ echo.
 @echo                 Dang Cai Dat Foxit Reader. Vui Long Cho
 @echo off
 
-taskkill /F /IM Foxit.exe
+taskkill /F /IM FoxitPDFReader.exe
 if exist %Windir%\SysWOW64 goto X64
 
-if exist Foxit*.exe goto I32
-if not exist Foxit*.exe goto D32
+if exist FoxitPDFReader*32*.exe goto I32
+if not exist FoxitPDFReader*32*.exe goto D32
 
 :D32
-@echo Dang Tai Xuong...
-powershell -Command "Invoke-WebRequest -Uri 'https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Windows&version=&package_type=&language=English&distID=' -OutFile 'FoxitReaderSetup_HieuckIT.exe'"
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong Foxit Reader...
+powershell -Command "Invoke-WebRequest -Uri ''https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Windows&version=&package_type=&language=English&distID=' -OutFile 'FoxitPDFReader-32_HieuckIT.exe'"
+@echo Tai Xuong Foxit Reader Hoan Thanh.
 goto I32
 
 :I32
-@echo Dang Cai Dat...
-FOR %%i IN ("Foxit*Reader*Setup*.exe") DO Set FileName="%%i"
-%FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-@echo Cai Dat Thanh Cong.
+@echo Dang Cai Dat Foxit Reader...
+FOR %%i IN ("FoxitPDFReader*32*.exe") DO Set FileName="%%i"
+%FileName% /S
+@echo Cai Dat Foxit Reader Thanh Cong.
 goto Lic
 
 :X64
-if  exist Foxit*.exe goto I64
-if not exist Foxit*.exe goto D64
+if  exist FoxitPDFReader*64*.exe goto I64
+if not exist FoxitPDFReader*64*.exe goto D64
 
 :D64
-@echo Dang Tai Xuong...
-powershell -Command "Invoke-WebRequest -Uri 'https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Windows&version=&package_type=&language=English&distID=' -OutFile 'FoxitReaderSetup_HieuckIT.exe'"
-@echo Tai Xuong Hoan Thanh.
+@echo Dang Tai Xuong Foxit Reader...
+powershell -Command "Invoke-WebRequest -Uri 'https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Windows&version=&package_type=&language=English&distID=' -OutFile 'FoxitPDFReader-64_HieuckIT.exe'"
+@echo Tai Xuong Foxit Reader Hoan Thanh.
 goto I64
 
 :I64
-@echo Dang Cai Dat...
-FOR %%i IN ("Foxit*Reader*Setup*.exe") DO Set FileName="%%i"
-%FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-@echo Cai Dat Thanh Cong.
+@echo Dang Cai Dat Foxit Reader...
+FOR %%i IN ("FoxitPDFReader*64*.exe") DO Set FileName="%%i"
+%FileName% /S
+@echo Cai Dat Foxit Reader Thanh Cong.
 goto Lic
 
 :Lic
-::copy /y "ban_quyen_neu_co" "vao_day"
+::copy /y "banquyenneuco" "vaoday"
 goto END
 
 :END
-del Foxit*.exe
+del FoxitPDFReader*.exe
 echo.
 echo Installation completed successfully
