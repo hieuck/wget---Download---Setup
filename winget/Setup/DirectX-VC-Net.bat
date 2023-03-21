2451e48d5aaa91269a23d3f4b8c36f0c6a5cd30d
hieuck <hieuck@users.noreply.github.com>
2023-03-21 07:22:18 +0700

    Create DirectX-VC-Net.bat

---
+++ winget/Setup/DirectX-VC-Net.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat DirectX - VCRedist - NetFx3 Tu Dong.
+@echo off
+winget install -e --id Microsoft.DirectX
+winget install -e --id Microsoft.VCRedist.2005.x86
+winget install -e --id Microsoft.VCRedist.2005.x64
+winget install -e --id Microsoft.VCRedist.2008.x86
+winget install -e --id Microsoft.VCRedist.2008.x64
+winget install -e --id Microsoft.VCRedist.2010.x86
+winget install -e --id Microsoft.VCRedist.2010.x64
+winget install -e --id Microsoft.VCRedist.2012.x86
+winget install -e --id Microsoft.VCRedist.2012.x64
+winget install -e --id Microsoft.VCRedist.2013.x86
+winget install -e --id Microsoft.VCRedist.2013.x64
+winget install -e --id  Microsoft.VCRedist.2015+.x86
+winget install -e --id Microsoft.VCRedist.2015+.x64
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All
