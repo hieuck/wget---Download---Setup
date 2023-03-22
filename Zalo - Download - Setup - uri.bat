8e2917bd7b97c097731d11c214fe4d3908b4dee5
hieuck <hieuck@users.noreply.github.com>
2023-03-22 20:15:58 +0700

    Update Zalo - Download - Setup - uri.bat

--- Zalo - Download - Setup - uri.bat
+++ Zalo - Download - Setup - uri.bat
@@ -18,22 +18,23 @@ echo.
 @pushd "%UserProfile%\Downloads"
 @cd /d "%UserProfile%\Downloads"
 taskkill /F /IM Zalo.exe
+
 if exist %Windir%\SysWOW64 goto X64
 
 if exist Zalo*.exe goto I32
 if not exist Zalo*.exe goto D32
 
 :D32
-@echo Dang Tai Xuong...
+@echo Dang Tai Xuong Zalo...
 powershell -Command "Invoke-WebRequest -Uri 'https://zalo.me/download/zalo-pc?utm=90000' -OutFile 'Zalo_HieuckIT.exe'"
 @echo Tai Xuong Hoan Thanh.
 goto I32
 
 :I32
-@echo Dang Cai Dat...
+@echo Dang Cai Dat Zalo...
 FOR %%i IN ("Zalo*.exe") DO Set FileName="%%i"
 %FileName% /S
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat Zalo Thanh Cong.
 goto Lic
 
 :X64
@@ -42,16 +43,16 @@ if  exist Zalo*.exe goto I64
 if not exist Zalo*.exe goto D64
 
 :D64
-@echo Dang Tai Xuong...
+@echo Dang Tai Xuong Zalo...
 powershell -Command "Invoke-WebRequest -Uri 'https://zalo.me/download/zalo-pc?utm=90000' -OutFile 'Zalo_HieuckIT.exe'"
 @echo Tai Xuong Hoan Thanh.
 goto I64
 
 :I64
-@echo Dang Cai Dat...
+@echo Dang Cai Dat Zalo...
 FOR %%i IN ("Zalo*.exe") DO Set FileName="%%i"
 %FileName% /S
-@echo Cai Dat Thanh Cong.
+@echo Cai Dat Zalo Thanh Cong.
 goto Lic
 
 :Lic
@@ -59,6 +60,5 @@ goto Lic
 goto END
 
 :END
-del Zalo*.exe
 echo.
 echo Installation completed successfully
