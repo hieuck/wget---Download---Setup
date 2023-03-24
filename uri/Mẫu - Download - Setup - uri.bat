e7fa794bba146c47fed34b3cbcd4a3bdff09de18
hieuck <hieuck@users.noreply.github.com>
2023-03-24 20:42:21 +0700

    Create Mẫu - Download - Setup - uri.bat

---
+++ uri/Mẫu - Download - Setup - uri.bat
@@ -0,0 +1,50 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat tenphanmem. Vui Long Cho
+@echo off
+
+pushd "%~dp0"
+taskkill /F /IM "tenkill.exe"
+:: Detect Windows architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+    set "ARCH=x64"
+) else (
+    set "ARCH=x86"
+)
+
+:: Download
+echo Downloading tenphanmem...
+if %ARCH%==x64 (
+    powershell -Command "Invoke-WebRequest -Uri 'link64' -OutFile 'tentep-64_HieuckIT.exe'"
+) else (
+    powershell -Command "Invoke-WebRequest -Uri ''link32' -OutFile 'tentep-32_HieuckIT.exe'"
+)
+
+:: Install
+echo Installing tenphanmem...
+FOR %%i IN ("tentep*.exe") DO Set FileName="%%i"
+%FileName% /S
+if exist "%ProgramFiles%\path\tenkill.exe" (
+	echo Installation tenphanmem complete.
+) else (
+	echo Installation tenphanmem failed.
+)
+::License
+::copy /y "banquyenneuco" "vaoday"
+
+:: Clean up
+del "tentep*.exe"
+timeout /t 5
+popd
