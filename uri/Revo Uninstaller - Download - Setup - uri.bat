974b413e9f398476395e20915e2cc598b3a4c480
hieuck <hieuck@users.noreply.github.com>
2023-03-24 20:42:20 +0700

    Create Revo Uninstaller - Download - Setup - uri.bat

---
+++ uri/Revo Uninstaller - Download - Setup - uri.bat
@@ -0,0 +1,61 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat Coc Coc. Vui Long Cho
+@echo off
+
+taskkill /F /IM RevoUninPro.exe
+if exist %Windir%\SysWOW64 goto X64
+
+if exist Revo*32*.exe goto I32
+if not exist Revo*32*.exe goto D32
+
+:D32
+@echo Dang Tai Xuong...
+powershell -Command "Invoke-WebRequest -Uri ''https://download.revouninstaller.com/download/RevoUninProSetup.exe' -OutFile 'Revo_32_HieuckIT.exe'"
+@echo Tai Xuong Hoan Thanh.
+goto I32
+
+:I32
+@echo Dang Cai Dat...
+FOR %%i IN ("Revo*32*.exe") DO Set FileName="%%i"
+%FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+@echo Cai Dat Thanh Cong.
+goto Lic
+
+:X64
+if  exist Revo*64*.exe goto I64
+if not exist Revo*64*..exe goto D64
+
+:D64
+@echo Dang Tai Xuong...
+powershell -Command "Invoke-WebRequest -Uri 'https://download.revouninstaller.com/download/RevoUninProSetup.exe' -OutFile 'Revo_64_HieuckIT.exe'"
+@echo Tai Xuong Hoan Thanh.
+goto I64
+
+:I64
+@echo Dang Cai Dat...
+FOR %%i IN ("Revo*64*.exe") DO Set FileName="%%i"
+%FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+@echo Cai Dat Thanh Cong.
+goto Lic
+
+:Lic
+copy /y "revouninstallerpro5.lic" "%PROGRAMDATA%\VS Revo Group\Revo Uninstaller Pro\"
+goto END
+
+:END
+del Revo*.exe
+echo.
+echo Installation completed successfully
