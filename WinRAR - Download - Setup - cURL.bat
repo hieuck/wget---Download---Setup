e871f6434199c2ac74765e15a4509d252f7e6437
hieuck <hieuck@users.noreply.github.com>
2023-03-24 20:42:07 +0700

    Create WinRAR - Download - Setup - cURL.bat

---
+++ WinRAR - Download - Setup - cURL.bat
@@ -0,0 +1,58 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=20
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat WinRAR. Vui Long Cho
+@echo off
+
+pushd "%~dp0"
+taskkill /F /IM "WinRAR.exe"
+:: Detect Windows architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+    set "ARCH=x64"
+) else (
+    set "ARCH=x86"
+)
+
+:: Set user agent
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Download
+echo Downloading WinRAR...
+if %ARCH%==x64 (
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "WinRAR-HieuckIT.exe" "https://www.rarlab.com/rar/winrar-x64-621.exe"
+) else (
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "WinRAR-HieuckIT.exe" "https://www.rarlab.com/rar/winrar-x32-621.exe"
+)
+
+:: Install
+echo Installing WinRAR...
+FOR %%i IN ("WinRAR*.exe") DO Set FileName="%%i"
+%FileName% /S
+if exist "%ProgramFiles%\WinRAR\WinRAR.exe" (
+	echo Installation WinRAR complete.
+) else (
+	echo Installation WinRAR failed.
+)
+::License
+copy /y "%~dp0\WinRAR Cr4ck\rarreg.key" "%ProgramFiles%\WinRAR"
+if exist "%ProgramFiles%\WinRAR\rarreg.key" (
+	echo Cr4ck WinRAR complete.
+) else (
+	echo Cr4ck WinRAR failed.
+)
+
+:: Clean up
+del "WinRAR*.exe"
+timeout /t 5
+popd
