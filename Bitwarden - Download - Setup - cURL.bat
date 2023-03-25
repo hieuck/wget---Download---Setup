c2405a59a487f468bdee435fccb6f78413d11456
hieuck <hieuck@users.noreply.github.com>
2023-03-25 12:54:42 +0700

    Create Bitwarden - Download - Setup - cURL.bat

---
+++ Bitwarden - Download - Setup - cURL.bat
@@ -0,0 +1,53 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=20
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat Bitwarden. Vui Long Cho
+@echo off
+
+pushd "%~dp0"
+taskkill /F /IM "Bitwarden.exe"
+:: Detect Windows architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+    set "ARCH=x64"
+) else (
+    set "ARCH=x86"
+)
+
+:: Set user agent
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Download
+echo Downloading Bitwarden...
+if %ARCH%==x64 (
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Bitwarden-HieuckIT.exe" "https://vault.bitwarden.com/download/?app=desktop&platform=windows"
+) else (
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Bitwarden-HieuckIT.exe" "link32"
+)
+
+:: Install
+echo Installing Bitwarden...
+FOR %%i IN ("Bitwarden*.exe") DO Set FileName="%%i"
+%FileName% /S
+if exist "%LocalAppData%\Programs\Bitwarden\Bitwarden.exe" (
+	echo Installation Bitwarden complete.
+) else (
+	echo Installation Bitwarden failed.
+)
+::License
+::copy /y "%~dp0\banquyenneuco" "vaoday"
+
+:: Clean up
+del "Bitwarden*.exe"
+timeout /t 5
+popd
