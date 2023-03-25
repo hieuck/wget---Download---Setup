7eaf4b6ed6f649708bbf953ac512fde603b0f230
hieuck <hieuck@users.noreply.github.com>
2023-03-25 12:55:05 +0700

    Update Topaz Video AI - Download - Setup - cURL.bat

--- Topaz Video AI - Download - Setup - cURL.bat
+++ Topaz Video AI - Download - Setup - cURL.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
-mode con:cols=100 lines=15
+mode con:cols=100 lines=20
 @cls
 echo.
 echo.
@@ -17,32 +17,37 @@ echo.
 
 pushd "%~dp0"
 taskkill /F /IM "Topaz Video AI.exe"
+:: Detect Windows architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+    set "ARCH=x64"
+) else (
+    set "ARCH=x86"
+)
 
-if  exist TopazVideoAI*.msi goto Install
-if not exist TopazVideoAI*.msi goto Download
+:: Set user agent
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
-:Download
-@echo Dang Tai Xuong Topaz Video AI...
-curl -L --max-redirs 20 -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" -o TopazVideoAI-HieuckIT.msi https://topazlabs.com/d/tvai/latest/win/full
-@echo Tai Xuong Topaz Video AI Hoan Thanh.
-goto Install
+:: Download
+echo Downloading Topaz Video AI...
+if %ARCH%==x64 (
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "TopazVideoAI-HieuckIT.msi" "https://topazlabs.com/d/tvai/latest/win/full"
+) else (
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "TopazVideoAI-HieuckIT.exe" "link32"
+)
 
-:Install
-@echo Dang Cai Dat Topaz Video AI...
+:: Install
+echo Installing Topaz Video AI...
 FOR %%i IN ("TopazVideoAI*.msi") DO Set FileName="%%i"
 %FileName% /quiet /norestart
-if not exist "%ProgramFiles%\Topaz Labs LLC\Topaz Video AI\Topaz Video AI.exe" (
-	@echo Cai Dat Topaz Video AI That Bai. Dang Thu Lai...
-	goto Install
+if exist "%ProgramFiles%\Topaz Labs LLC\Topaz Video AI\Topaz Video AI.exe" (
+	echo Installation Topaz Video AI complete.
+) else (
+	echo Installation Topaz Video AI failed.
 )
-@echo Cai Dat Topaz Video AI Thanh Cong.
-goto Lic
-
-:Lic
+::License
 copy /y "%~dp0\Topaz Video AI Cr4ck\" "%ProgramFiles%\Topaz Labs LLC\Topaz Video AI"
-goto END
 
-:END
-::del TopazVideoAI*.msi
-echo.
-echo Installation completed successfully
+:: Clean up
+del "TopazVideoAI*.msi"
+timeout /t 5
+popd
