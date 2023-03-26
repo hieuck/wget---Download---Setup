ab4c7727ab433591d052f88495e425f2bec13b1b
hieuck <hieuck@users.noreply.github.com>
2023-03-26 16:00:02 +0700

    Update Discord - Download - Setup - cURL.bat

--- Discord - Download - Setup - cURL.bat
+++ Discord - Download - Setup - cURL.bat
@@ -12,48 +12,54 @@ echo.
 @echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
 @echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
 @echo.  
-@echo                 Dang Cai Dat Discord . Vui Long Cho
+@echo                 Dang Cai Dat Discord. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the Discord  process
-tasklist | find /i "Discord .exe" > nul
-if %errorlevel% equ 0 (
-    taskkill /im Discord .exe /f
-)
 
-:: Detect Windows architecture
+:: Set File Name Link User Agent
+set "FILENAME=Discord-HieuckIT.exe"
+set "LINK64=https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
-echo Downloading Discord ...
+echo Downloading Discord...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Discord -HieuckIT.exe" "https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Discord -HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download Discord failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
-echo Installing Discord ...
-FOR %%i IN ("Discord *.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%LocalAppData%\Discord\" (
-	echo Installation Discord  complete.
-	::License
-	::copy /y "%~dp0\banquyenneuco" "vaoday"
-) else (
-	echo Installation Discord  failed.
+echo Installing Discord...
+"%FILENAME%" /S
+
+:: Check Installation Process
+if not exist "%LocalAppData%\Discord\app.ico" (
+	echo Installation Discord failed.
 	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation Discord complete.
 )
 
-:: Clean up
-del "Discord *.exe"
-timeout /t 5
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
