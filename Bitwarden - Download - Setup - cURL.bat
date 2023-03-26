609b7cfe90bcd20eb57f533428f119d4e951f420
hieuck <hieuck@users.noreply.github.com>
2023-03-26 15:35:13 +0700

    Update Bitwarden - Download - Setup - cURL.bat

--- Bitwarden - Download - Setup - cURL.bat
+++ Bitwarden - Download - Setup - cURL.bat
@@ -14,45 +14,58 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat Bitwarden. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
+
+:: Set File Name Link User Agent
+set "FILENAME=Bitwarden-HieuckIT.exe"
+set "LINK64=https://vault.bitwarden.com/download/?app=desktop&platform=windows"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the Bitwarden Process
 tasklist | find /i "Bitwarden.exe" > nul
 if %errorlevel% equ 0 (
-    taskkill /im Bitwarden.exe /f
+    taskkill /im "Bitwarden.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading Bitwarden...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Bitwarden-HieuckIT.exe" "https://vault.bitwarden.com/download/?app=desktop&platform=windows"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Bitwarden-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download Bitwarden failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing Bitwarden...
-FOR %%i IN ("Bitwarden*.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%LocalAppData%\Programs\Bitwarden\Bitwarden.exe" (
-	echo Installation Bitwarden complete.
-) else (
+"%FILENAME%" /S
+
+:: Check Installation Process
+if not exist "%LocalAppData%\Programs\Bitwarden\Bitwarden.exe" (
 	echo Installation Bitwarden failed.
 	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation Bitwarden complete.
 )
-::License
-::copy /y "%~dp0\banquyenneuco" "vaoday"
 
-:: Clean up
-del "Bitwarden*.exe"
-timeout /t 5
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
