3511a71ba037b494e6e083dfbb4def1c054c936d
hieuck <hieuck@users.noreply.github.com>
2023-03-26 14:54:40 +0700

    Update WinRAR - Download - Setup - cURL.bat

--- WinRAR - Download - Setup - cURL.bat
+++ WinRAR - Download - Setup - cURL.bat
@@ -14,52 +14,68 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat WinRAR. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the WinRAR process
+
+:: Set File Name Link User Agent
+set "FILENAME=WinRAR-HieuckIT.exe"
+set "LINK64=https://www.rarlab.com/rar/winrar-x64-621.exe"
+set "LINK32=https://www.rarlab.com/rar/winrar-x32-621.exe"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the WinRAR Process
 tasklist | find /i "WinRAR.exe" > nul
 if %errorlevel% equ 0 (
-    taskkill /im WinRAR.exe /f
+    taskkill /im "WinRAR.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading WinRAR...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "WinRAR-HieuckIT.exe" "https://www.rarlab.com/rar/winrar-x64-621.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "WinRAR-HieuckIT.exe" "https://www.rarlab.com/rar/winrar-x32-621.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download WinRAR failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing WinRAR...
-FOR %%i IN ("WinRAR*.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%ProgramFiles%\WinRAR\WinRAR.exe" (
-	echo Installation WinRAR complete.
-	::License
-	copy /y "%~dp0\WinRAR Cr4ck\rarreg.key" "%ProgramFiles%\WinRAR"
-	if exist "%ProgramFiles%\WinRAR\rarreg.key" (
-		echo Cr4ck WinRAR complete.
-	) else (
-		echo Cr4ck WinRAR failed.
-	)
+%FILENAME% /S
 
-) else (
+:: Check Installation Process
+if not exist "%ProgramFiles%\WinRAR\WinRAR.exe" (
 	echo Installation WinRAR failed.
 	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+	exit
+) else (
+	echo Installation WinRAR complete.
 )
 
-:: Clean up
-del "WinRAR*.exe"
-timeout /t 5
+:: License
+copy /y "%~dp0\WinRAR Cr4ck\rarreg.key" "%ProgramFiles%\WinRAR"	
+if exist "%ProgramFiles%\WinRAR\rarreg.key" (
+	echo Cr4ck WinRAR complete.
+) else (
+	echo Cr4ck WinRAR failed.
+	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+	exit
+)
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
