0dac7de519840f85b3afcd77fbf0cae62a93ffe1
hieuck <hieuck@users.noreply.github.com>
2023-03-26 17:36:56 +0700

    Update Microsoft OneDrive - Download - Setup - cURL.bat

--- Microsoft OneDrive - Download - Setup - cURL.bat
+++ Microsoft OneDrive - Download - Setup - cURL.bat
@@ -14,44 +14,58 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat OneDrive. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the OneDrive process
+
+:: Set File Name Link User Agent
+set "FILENAME=OneDrive-HieuckIT.exe"
+set "LINK64=https://oneclient.sfx.ms/Win/Prod/22.131.0619.0001/amd64/OneDriveSetup.exe"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the OneDrive Process
 tasklist | find /i "OneDrive.exe" > nul
 if %errorlevel% equ 0 (
-    taskkill /im OneDrive.exe /f
+    taskkill /im "OneDrive.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading OneDrive...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "OneDrive-HieuckIT.exe" "https://oneclient.sfx.ms/Win/Prod/22.131.0619.0001/amd64/OneDriveSetup.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "OneDrive-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download OneDrive failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing OneDrive...
-FOR %%i IN ("OneDrive*.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%ProgramFiles%\Microsoft OneDrive\" (
-	echo Installation OneDrive complete.
-) else (
+"%FILENAME%" /S
+
+:: Check Installation Process
+if not exist "%ProgramFiles%\Microsoft OneDrive\OneDrive.exe" (
 	echo Installation OneDrive failed.
-	echo Please try Run as Administrator.
+	echo Please try Run as Administrator. Exiting in 5 seconds...
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation OneDrive complete.
 )
 
-:: Clean up
-del "OneDrive*.exe"
-timeout /t 5
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
