70ebeb36dbdbf30a926ce35ddb0b8870fea2ece4
hieuck <hieuck@users.noreply.github.com>
2023-03-26 15:59:13 +0700

    Update DirectX - Download - Setup - cURL.bat

--- DirectX - Download - Setup - cURL.bat
+++ DirectX - Download - Setup - cURL.bat
@@ -14,38 +14,59 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat DirectX End-User Runtime. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the DirectX End-User Runtime process
-tasklist | find /i "tenkill.exe" > nul
-if %errorlevel% equ 0 (
-    taskkill /im tenkill.exe /f
-)
 
-:: Detect Windows architecture
+:: Set File Name Link User Agent
+set "FILENAME=DirectX End-User Runtime-HieuckIT.exe"
+set "LINK64=https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading DirectX End-User Runtime...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "DirectX-HieuckIT.exe" "https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "DirectX-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download DirectX End-User Runtime failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing DirectX End-User Runtime...
-FOR %%i IN ("DirectX*.exe") DO Set FileName="%%i"
-%FileName%  /Q
+"%FILENAME%" /Q
 
-:: Clean up
-del "DirectX*.exe"
-timeout /t 5
+setlocal
+
+:: Check DirectX version
+reg query "HKLM\Software\Microsoft\DirectX" /v Version > nul 2>&1
+if %errorlevel% equ 0 (
+    for /f "tokens=3" %%i in ('reg query "HKLM\Software\Microsoft\DirectX" /v Version') do (
+        set directx_version=%%i
+    )
+)
+
+:: Compare DirectX version with maximum supported version
+if "%directx_version%" geq "4.09.00.0904" (
+    echo DirectX End-User Runtime is up to date.
+) else (
+    echo DirectX End-User Runtime is not up to date.
+)
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
