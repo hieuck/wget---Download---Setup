a84b247ff88a376f5e9912ad90c465d1456838ae
hieuck <hieuck@users.noreply.github.com>
2023-03-26 16:02:45 +0700

    Update Foxit PDF Reader - Download - Setup - cURL.bat

--- Foxit PDF Reader - Download - Setup - cURL.bat
+++ Foxit PDF Reader - Download - Setup - cURL.bat
@@ -14,42 +14,58 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat Foxit PDF Reader. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
+
+:: Set File Name Link User Agent
+set "FILENAME=Foxit PDF Reader-HieuckIT.exe"
+set "LINK64=https://www.foxit.com/downloads/latest.html?product=Foxit-Reader"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the Foxit PDF Reader Process
 tasklist | find /i "FoxitPDFReader.exe" > nul
 if %errorlevel% equ 0 (
-    taskkill /im FoxitPDFReader.exe /f
+    taskkill /im "FoxitPDFReader.exe" /f
 )
-:: Detect Windows architecture
+
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading Foxit PDF Reader...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "FoxitPDFReader-HieuckIT.exe" "https://www.foxit.com/downloads/latest.html?product=Foxit-Reader"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "FoxitPDFReader-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download Foxit PDF Reader failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing Foxit PDF Reader...
-FOR %%i IN ("FoxitPDFReader*.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%ProgramFiles(x86)%\Foxit Software\Foxit PDF Reader\FoxitPDFReader.exe" (
-	echo Installation Foxit PDF Reader complete.
-) else (
+"%FILENAME%" /S
+
+:: Check Installation Process
+if not exist "%ProgramFiles(x86)%\Foxit Software\Foxit PDF Reader\FoxitPDFReader.exe" (
 	echo Installation Foxit PDF Reader failed.
 	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation Foxit PDF Reader complete.
 )
 
-:: Clean up
-del "FoxitPDFReader*.exe"
-timeout /t 5
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
