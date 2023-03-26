ff758473dd014115e7b4f153b670f8af9cc5ec24
hieuck <hieuck@users.noreply.github.com>
2023-03-26 17:33:49 +0700

    Update Dropbox - Download - Setup - cURL.bat

--- Dropbox - Download - Setup - cURL.bat
+++ Dropbox - Download - Setup - cURL.bat
@@ -14,44 +14,58 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat Dropbox. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the Dropbox process
+
+:: Set File Name Link User Agent
+set "FILENAME=Dropbox-HieuckIT.exe"
+set "LINK64=https://www.dropbox.com/download?os=win"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the Dropbox Process
 tasklist | find /i "Dropbox.exe" > nul
 if %errorlevel% equ 0 (
     taskkill /im "Dropbox.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading Dropbox...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Dropbox-HieuckIT.exe" "https://www.dropbox.com/download?os=win"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "Dropbox-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download Dropbox failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing Dropbox...
-FOR %%i IN ("Dropbox*.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%ProgramFiles(x86)%\Dropbox\Client\Dropbox.exe" (
-	echo Installation Dropbox complete.
-) else (
+"%FILENAME%" /S
+
+:: Check Installation Process
+if not exist "%ProgramFiles(x86)%\Dropbox\Client\Dropbox.exe" (
 	echo Installation Dropbox failed.
-	echo Please try Run as Administrator.
+	echo Please try Run as Administrator. Exiting in 5 seconds...
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation Dropbox complete.
 )
 
-:: Clean up
-del "Dropbox*.exe"
-timeout /t 5
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
