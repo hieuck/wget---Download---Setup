b0a41627d9290f94f3dcb39921c77a31e3b83a86
hieuck <hieuck@users.noreply.github.com>
2023-03-26 17:22:56 +0700

    Update K-Lite - Download - Setup - cURL.bat

--- K-Lite - Download - Setup - cURL.bat
+++ K-Lite - Download - Setup - cURL.bat
@@ -14,46 +14,58 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat K-Lite Codec Pack Mega. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the K-Lite Codec Pack Mega process
-tasklist | find /i "tenkill.exe" > nul
+
+:: Set File Name Link User Agent
+set "FILENAME=K-Lite Codec Pack Mega-HieuckIT.exe"
+set "LINK64=https://files3.codecguide.com/K-Lite_Codec_Pack_1750_Mega.exe"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the K-Lite Codec Pack Mega Process
+tasklist | find /i "mpc-hc64.exe" > nul
 if %errorlevel% equ 0 (
-    taskkill /im tenkill.exe /f
+    taskkill /im "mpc-hc64.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading K-Lite Codec Pack Mega...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "K-Lite-HieuckIT.exe" "https://files3.codecguide.com/K-Lite_Codec_Pack_1750_Mega.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "K-Lite-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download K-Lite Codec Pack Mega failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing K-Lite Codec Pack Mega...
-FOR %%i IN ("K-Lite*.exe") DO Set FileName="%%i"
-%FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-if exist "%ProgramFiles(x86)%\K-Lite Codec Pack\" (
-	echo Installation K-Lite Codec Pack Mega complete.
-) else (
+"%FILENAME%" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+
+:: Check Installation Process
+if not exist "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe" (
 	echo Installation K-Lite Codec Pack Mega failed.
 	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation K-Lite Codec Pack Mega complete.
 )
-::License
-::copy /y "%~dp0\banquyenneuco" "vaoday"
 
-:: Clean up
-del "K-Lite*.exe"
-timeout /t 5
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
