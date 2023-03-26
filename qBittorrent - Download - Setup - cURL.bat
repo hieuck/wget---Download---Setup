d31c8763c57460ba31c3747715acfc8c86e53a1c
hieuck <hieuck@users.noreply.github.com>
2023-03-26 17:39:22 +0700

    Update qBittorrent - Download - Setup - cURL.bat

--- qBittorrent - Download - Setup - cURL.bat
+++ qBittorrent - Download - Setup - cURL.bat
@@ -14,46 +14,58 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat qBittorrent. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the qBittorrent process
+
+:: Set File Name Link User Agent
+set "FILENAME=qBittorrent-HieuckIT.exe"
+set "LINK64=https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.5.2_lt20_qt6_x64_setup.exe"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the qBittorrent Process
 tasklist | find /i "qBittorrent.exe" > nul
 if %errorlevel% equ 0 (
-    taskkill /im qBittorrent.exe /f
+    taskkill /im "qBittorrent.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading qBittorrent...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "qBittorrent-HieuckIT.exe" "https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.5.2_lt20_qt6_x64_setup.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "qBittorrent-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download qBittorrent failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing qBittorrent...
-FOR %%i IN ("qBittorrent*.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%ProgramFiles%\qBittorrent\" (
-	echo Installation qBittorrent complete.
-) else (
-	echo Installation qBittorrent failed.
-	echo Please try Run as Administrator.
-)
-::License
-::copy /y "%~dp0\banquyenneuco" "vaoday"
+"%FILENAME%" /S
 
-:: Clean up
-del "qBittorrent*.exe"
-timeout /t 5
+:: Check Installation Process
+if not exist "%ProgramFiles%\qBittorrent\qbittorrent.exe" (
+	echo Installation qBittorrent failed.
+	echo Please try Run as Administrator. Exiting in 5 seconds...
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation qBittorrent complete.
+)
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
