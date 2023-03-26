89f1f03c6e7f1bcc6db55280c57d050523b86735
hieuck <hieuck@users.noreply.github.com>
2023-03-26 15:41:27 +0700

    Update CocCoc - Download - Setup - cURL.bat

--- CocCoc - Download - Setup - cURL.bat
+++ CocCoc - Download - Setup - cURL.bat
@@ -14,44 +14,58 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat Coc Coc. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
+
+:: Set File Name Link User Agent
+set "FILENAME=Coc Coc-HieuckIT.exe"
+set "LINK64=https://files-cdn.coccoc.com/browser/x64/coccoc_vi_machine.exe"
+set "LINK32=https://files-cdn.coccoc.com/browser/coccoc_vi_machine.exe"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the Coc Coc Process
 tasklist | find /i "browser.exe" > nul
 if %errorlevel% equ 0 (
-    taskkill /im browser.exe /f
+    taskkill /im "browser.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading Coc Coc...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -O "https://files-cdn.coccoc.com/browser/x64/coccoc_vi_machine.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -O "https://files-cdn.coccoc.com/browser/coccoc_vi_machine.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+    echo Download Coc Coc failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
 )
 
 :: Install
 echo Installing Coc Coc...
-FOR %%i IN ("coccoc*.exe") DO Set FileName="%%i"
-%FileName% /install
-if exist "%ProgramFiles%\CocCoc\Browser\Application\browser.exe" (
-	echo Installation Coc Coc complete.
-) else (
-	echo Installation Coc Coc failed.
-)
-::License
-::copy /y "%~dp0\banquyenneuco" "vaoday"
+"%FILENAME%" /install
 
-:: Clean up
-del "coccoc*.exe"
-timeout /t 5
+:: Check Installation Process
+if not exist "%ProgramFiles%\CocCoc\Browser\Application\browser.exe" (
+	echo Installation Coc Coc failed.
+	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+	exit
+) else (
+	echo Installation Coc Coc complete.
+)
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
