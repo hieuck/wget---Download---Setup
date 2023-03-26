8988a863c02d398fd4b15e95e8afde0764a62a21
hieuck <hieuck@users.noreply.github.com>
2023-03-26 15:18:30 +0700

    Update Foxit PDF Editor - Download - Setup - cURL.bat

--- Foxit PDF Editor - Download - Setup - cURL.bat
+++ Foxit PDF Editor - Download - Setup - cURL.bat
@@ -14,51 +14,61 @@ echo.
 @echo.  
 @echo                 Dang Cai Dat Foxit PDF Editor. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the Foxit PDF Editor process
+
+:: Set File Name Link User Agent
+set "FILENAME=Foxit PDF Editor-HieuckIT.exe"
+set "LINK64=https://cdn01.foxitsoftware.com/product/phantomPDF/desktop/win/12.1.1/FoxitPDFEditor1211_L10N_Setup_Website.exe"
+set "LINK32=link"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Terminate the Foxit PDF Editor Process
 tasklist | find /i "FoxitPDFEditor.exe" > nul
 if %errorlevel% equ 0 (
     taskkill /im "FoxitPDFEditor.exe" /f
 )
 
-:: Detect Windows architecture
+:: Detect Windows Architecture
 if exist "%SYSTEMROOT%\SysWOW64" (
     set "ARCH=x64"
 ) else (
     set "ARCH=x86"
 )
 
-::Check Install File
-if exist "FoxitPDFEditor*HieuckIT.exe" (
-	goto Install
-)
-
-:: Set user agent
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
 :: Download
 echo Downloading Foxit PDF Editor...
 if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "FoxitPDFEditor-HieuckIT.exe" "https://cdn01.foxitsoftware.com/product/phantomPDF/desktop/win/12.1.1/FoxitPDFEditor1211_L10N_Setup_Website.exe"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "FoxitPDFEditor-HieuckIT.exe" "link32"
+    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
 )
 
-:Install
+if not exist "%FILENAME%" (
+    echo Download Foxit PDF Editor failed.
+    echo Please check your network connection. Exiting in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    exit
+)
+
+:: Install
 echo Installing Foxit PDF Editor...
-FOR %%i IN ("FoxitPDFEditor*.exe") DO Set FileName="%%i"
-%FileName% /q /norestart
-if exist "%ProgramFiles(x86)%\Foxit Software\Foxit PDF Editor\FoxitPDFEditor.exe" (
-	echo Installation Foxit PDF Editor complete.
-	::License
-	::copy /y "%~dp0\banquyenneuco" "vaoday"
-) else (
+"%FILENAME%" /q /norestart
+
+:: Check Installation Process
+if not exist "%ProgramFiles(x86)%\Foxit Software\Foxit PDF Editor\FoxitPDFEditor.exe" (
 	echo Installation Foxit PDF Editor failed.
 	echo Please try Run as Administrator.
+	timeout /t 5 /nobreak >nul
+    exit
+) else (
+	echo Installation Foxit PDF Editor complete.
 )
 
-:: Clean up
-del "FoxitPDFEditor*.exe"
-timeout /t 5
+:: License
+::copy /y "%~dp0\banquyenneuco" "vaoday"
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 5 seconds.
+timeout /t 5 /nobreak >nul
 popd
