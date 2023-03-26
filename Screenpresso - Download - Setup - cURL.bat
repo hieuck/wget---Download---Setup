62b784c73c89e2d6cb48d00f478657e3dcc341b0
hieuck <hieuck@users.noreply.github.com>
2023-03-26 21:18:00 +0700

    Update Screenpresso - Download - Setup - cURL.bat

--- Screenpresso - Download - Setup - cURL.bat
+++ Screenpresso - Download - Setup - cURL.bat
@@ -131,6 +131,23 @@ if %errorlevel% neq 0 (
 )
 
 :: Clean Up
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat Screenpresso. Vui Long Cho
+@echo off
+pushd "%~dp0"
 del "%FILENAME%"
 echo The script will automatically close in 5 seconds.
 for /l %%i in (5,-1,1) do (
