e9d5b1a481da755180fb9acf19123150709510e1
hieuck <hieuck@users.noreply.github.com>
2023-03-27 13:01:56 +0700

    Update Bitwarden - Download - Setup - wget.bat

--- Bitwarden - Download - Setup - wget.bat
+++ Bitwarden - Download - Setup - wget.bat
@@ -15,12 +15,19 @@ echo.
 @echo                 Dang Cai Dat Bitwarden. Vui Long Cho
 @echo off
 pushd "%~dp0"
-
 :: Set File Name Link User Agent
 set "FILENAME=Bitwarden-HieuckIT.exe"
 set "LINK64=https://vault.bitwarden.com/download/?app=desktop&platform=windows"
 set "LINK32=link"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Check if Command Prompt is running with administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	echo Command Prompt is running as Administrator.
+) else (
+	echo Command Prompt is not running as Administrator.
+)
 
 :: Terminate the Bitwarden Process
 tasklist | find /i "Bitwarden.exe" > nul
@@ -36,6 +43,23 @@ if exist "%SYSTEMROOT%\SysWOW64" (
 )
 
 :: Download
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat Bitwarden. Vui Long Cho
+@echo off
+pushd "%~dp0"
 echo Downloading Bitwarden...
 if %ARCH%==x64 (
     wget --no-check-certificate -q --show-progress -O "%FILENAME%" "%LINK64%"
@@ -51,21 +75,39 @@ if not exist "%FILENAME%" (
 )
 
 :: Install
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat Bitwarden. Vui Long Cho
+@echo off
+pushd "%~dp0"
 echo Installing Bitwarden...
 "%FILENAME%" /S
 
 :: Check Installation Process
-if not exist "%LocalAppData%\Programs\Bitwarden\Bitwarden.exe" (
-	echo Installation Bitwarden failed.
-	echo Please try Run as Administrator. Exiting in 5 seconds...
-	timeout /t 5 /nobreak >nul
-	exit
-) else (
+if exist "%LocalAppData%\Programs\Bitwarden\Bitwarden.exe" (
 	echo Installation Bitwarden complete.
+) else (
+	echo Installation Bitwarden failed.
+	echo Please try Run as Administrator.
 )
 
 :: Clean Up
 del "%FILENAME%"
 echo The script will automatically close in 5 seconds.
 timeout /t 5 /nobreak >nul
+	if exist "%FILENAME%" (
+	del "%FILENAME%"
+	)
 popd
