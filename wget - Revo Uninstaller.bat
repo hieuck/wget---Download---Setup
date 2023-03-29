7f2d659187ecf2255344062bd325329a235be6a0
hieuck <hieuck@users.noreply.github.com>
2023-03-29 07:17:27 +0700

    Update wget - Revo Uninstaller.bat

--- wget - Revo Uninstaller.bat
+++ wget - Revo Uninstaller.bat
@@ -12,20 +12,45 @@ echo.
 @echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
 @echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
 @echo.  
-@echo                 Dang Cai Dat Revo Uninstaller. Vui Long Cho
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
 pushd "%~dp0"
-
 :: Set File Name Link User Agent
+set "SOFTNAME=Revo Uninstaller"
 set "FILENAME=Revo Uninstaller-HieuckIT.exe"
+set "PROCESS=RevoUninPro.exe"
 set "LINK64=https://download.revouninstaller.com/download/RevoUninProSetup.exe"
-set "LINK32=link"
+set "LINK32=danvaoday"
+set "QUIETMODE=/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+set "Admin=Yes"
+set "SOFTLOCATION=%ProgramFiles%\VS Revo Group\Revo Uninstaller Pro\RevoUninPro.exe"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
-:: Terminate the Revo Uninstaller Process
-tasklist | find /i "RevoUninPro.exe" > nul
+:: Check if Command Prompt is running with administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	echo Command Prompt is running as Administrator.
+) else (
+	if "%Admin%"=="Yes" (
+		echo Please Run as Administrator. Exiting in 3 seconds...
+		for /l %%i in (3,-1,1) do (
+			echo Exiting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+		exit
+	) else (
+		echo Warning: This program may not function correctly without administrator privileges.
+		for /l %%i in (3,-1,1) do (
+			echo Starting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+	)
+)
+
+:: Terminate the %SOFTNAME% Process
+tasklist | find /i "%PROCESS%" > nul
 if %errorlevel% equ 0 (
-	taskkill /im "RevoUninPro.exe" /f
+	taskkill /im "%PROCESS%" /f
 )
 
 :: Detect Windows Architecture
@@ -36,17 +61,34 @@ if exist "%SYSTEMROOT%\SysWOW64" (
 )
 
 :: Download
-echo Downloading Revo Uninstaller...
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Downloading %SOFTNAME%...
 if %ARCH%==x64 (
-	wget --no-check-certificate -q --show-progress -O "%FILENAME%" "%LINK64%"
+	wget --no-check-certificate --show-progress -q -O "%FILENAME%" "%LINK64%"
 ) else (
-	wget --no-check-certificate -q --show-progress -O "%FILENAME%" "%LINK32%"
+	wget --no-check-certificate --show-progress -q -O "%FILENAME%" "%LINK32%"
 )
 
 if not exist "%FILENAME%" (
-	echo Download Revo Uninstaller failed.
-	echo Please check your network connection. Exiting in 5 seconds...
-	for /l %%i in (5,-1,1) do (
+	echo Download %SOFTNAME% failed.
+	echo Please check your network connection. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
 		echo Exiting in %%i seconds...
 		timeout /t 1 /nobreak >nul
 	)
@@ -54,32 +96,40 @@ if not exist "%FILENAME%" (
 )
 
 :: Install
-echo Installing Revo Uninstaller...
-"%FILENAME%" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Installing %SOFTNAME%...
+"%FILENAME%" %QuietMode%
 
 :: Check Installation Process
-if not exist "%ProgramFiles%\VS Revo Group\Revo Uninstaller Pro\RevoUninPro.exe" (
-	echo Installation Revo Uninstaller failed.
-	echo Please try Run as Administrator. Exiting in 5 seconds...
-	for /l %%i in (5,-1,1) do (
-		echo Exiting in %%i seconds...
-		timeout /t 1 /nobreak >nul
-	)
-	exit
+if exist "%SoftLocation%" (
+	echo Installation %SOFTNAME% complete.
 ) else (
-	echo Installation Revo Uninstaller complete.
+	echo Installation %SOFTNAME% failed.
+	echo Please try Run as Administrator.
 )
 
 :: License
+echo Cr4cking %SOFTNAME%...
 del "%PROGRAMDATA%\VS Revo Group\Revo Uninstaller Pro\revouninstallerpro5.lic"
 if exist "%PROGRAMDATA%\VS Revo Group\Revo Uninstaller Pro\revouninstallerpro5.lic" (
 	echo Cr4cking Revo Uninstaller failed.
-	echo Please try Run as Administrator. Exiting in 5 seconds...
-	for /l %%i in (5,-1,1) do (
-		echo Exiting in %%i seconds...
-		timeout /t 1 /nobreak >nul
-	)
-	exit
+	echo Please try Run as Administrator.
 ) else (
 	copy /y "%~dp0\Revo Uninstaller Cr4ck\revouninstallerpro5.lic" "%PROGRAMDATA%\VS Revo Group\Revo Uninstaller Pro\"
 	if exist "%PROGRAMDATA%\VS Revo Group\Revo Uninstaller Pro\" (
@@ -89,9 +139,16 @@ if exist "%PROGRAMDATA%\VS Revo Group\Revo Uninstaller Pro\revouninstallerpro5.l
 
 :: Clean Up
 del "%FILENAME%"
-echo The script will automatically close in 5 seconds.
-for /l %%i in (5,-1,1) do (
+echo The script will automatically close in 3 seconds.
+for /l %%i in (3,-1,1) do (
 	echo Closing in %%i seconds...
 	timeout /t 1 /nobreak >nul
+	if exist "%FILENAME%" (
+		tasklist | find /i "%FILENAME%" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "%FILENAME%" /f
+		)
+	del "%FILENAME%"
+	)
 )
 popd
