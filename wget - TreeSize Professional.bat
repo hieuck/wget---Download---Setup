80e746dc4987ff03240ce65db9aba2fb95eccf44
hieuck <hieuck@users.noreply.github.com>
2023-03-29 16:55:32 +0700

    Update wget - TreeSize Professional.bat

--- wget - TreeSize Professional.bat
+++ wget - TreeSize Professional.bat
@@ -12,55 +12,151 @@ echo.
 @echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
 @echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
 @echo.  
-@echo                 Dang Cai Dat TreeSize Professional. Vui Long Cho
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the TreeSize Professional process
-tasklist | find /i "TreeSize.exe" > nul
-if %errorlevel% equ 0 (
-    taskkill /im TreeSize.exe /f
-)
-
-:: Detect Windows architecture
-if exist "%SYSTEMROOT%\SysWOW64" (
-    set "ARCH=x64"
-) else (
-    set "ARCH=x86"
-)
-
-:: Set user agent
+:: Set File Name Link User Agent
+set "SOFTNAME=TreeSize Professional"
+set "FILENAME=TreeSize Professional-HieuckIT.exe"
+set "PROCESS=TreeSize.exe"
+set "LINK64=https://downloads.jam-software.de/treesize/TreeSize-x64-Demo.exe"
+set "LINK32=danvaoday"
+set "QUIETMODE=/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+set "Admin="
+set "SOFTLOCATION=%ProgramFiles%\JAM Software\TreeSize\TreeSize.exe"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
-:: Download
-echo Downloading TreeSize Professional...
-if %ARCH%==x64 (
-    wget --no-check-certificate -q --show-progress "https://downloads.jam-software.de/treesize/TreeSize-x64-Demo.exe"
+:: Check if Command Prompt is running with administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	echo Command Prompt is running as Administrator.
 ) else (
-    wget --no-check-certificate -q --show-progress "link32"
+	if "%Admin%"=="Yes" (
+		echo Please Run as Administrator. Exiting in 3 seconds...
+		for /l %%i in (3,-1,1) do (
+			echo Exiting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+		exit
+	) else (
+		echo Warning: This program may not function correctly without administrator privileges.
+		for /l %%i in (3,-1,1) do (
+			echo Starting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+	)
+)
+
+:: Terminate the %SOFTNAME% Process
+tasklist | find /i "%PROCESS%" > nul
+if %errorlevel% equ 0 (
+	taskkill /im "%PROCESS%" /f
+)
+
+:: Detect Windows Architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+	set "ARCH=x64"
+) else (
+	set "ARCH=x86"
+)
+
+:: Download
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Downloading %SOFTNAME%...
+if %ARCH%==x64 (
+	wget --no-check-certificate --show-progress -q -O "%FILENAME%" "%LINK64%"
+) else (
+	wget --no-check-certificate --show-progress -q -O "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+	echo Download %SOFTNAME% failed.
+	echo Please check your network connection. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
 )
 
 :: Install
-echo Installing TreeSize Professional...
-FOR %%i IN ("TreeSize*.exe") DO Set FileName="%%i"
-%FileName% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-if exist "%ProgramFiles%\JAM Software\TreeSize\TreeSize.exe" (
-	echo Installation TreeSize Professional complete.
-	::License
-	if exist "%ProgramFiles%\JAM Software\TreeSize\TreeSize.exe" (
-		del "%ProgramFiles%\JAM Software\TreeSize\TreeSize.exe"
-		copy /y "%~dp0\TreeSize Professional Cr4ck\TreeSize.exe" "%ProgramFiles%\JAM Software\TreeSize\"
-		echo Cr4ck TreeSize Professional complete.
-	) else (
-		echo Cr4ck TreeSize Professional failed.
-		echo Please try Run as Administrator.
-	)
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Installing %SOFTNAME%...
+"%FILENAME%" %QUIETMODE%
+
+:: Check Installation Process
+if exist "%SOFTLOCATION%" (
+	echo Installation %SOFTNAME% complete.
 ) else (
-	echo Installation TreeSize Professional failed.
+	echo Installation %SOFTNAME% failed.
 	echo Please try Run as Administrator.
 )
 
-:: Clean up
-del "TreeSize*.exe"
-timeout /t 5
+:: License
+set "License=Yes"
+set "CR4CKFILE=TreeSize.exe"
+set "LINKCR4CK=https://github.com/hieuck/curl-uri-wget-download-setup/blob/main/TreeSize%20Professional%20Cr4ck/TreeSize.exe"
+set "SOFTPATH=%ProgramFiles%\JAM Software\TreeSize"
+if "%License%"=="Yes" (
+	echo Cracking %SOFTNAME%...
+	wget --no-check-certificate --show-progress -q -O "%CR4CKFILE%" "%LINKCR4CK%"
+	if exist "%CR4CKFILE%" (
+		move /y "%CR4CKFILE%" "%SOFTPATH%"
+	) else (
+		echo Please try running the script as Administrator.
+	)
+	if exist "%SOFTPATH%\%CR4CKFILE%" (
+		echo Successfully Cr4cked %SOFTNAME%.
+	) else (
+		echo Cr4cking %SOFTNAME% failed.
+		echo Please try running the script as Administrator.
+	)
+)
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 3 seconds.
+for /l %%i in (3,-1,1) do (
+	echo Closing in %%i seconds...
+	timeout /t 1 /nobreak >nul
+	if exist "%FILENAME%" (
+		tasklist | find /i "%FILENAME%" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "%FILENAME%" /f
+		)
+	del "%FILENAME%"
+	)
+)
 popd
