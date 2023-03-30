4fdbf8dfffd18b36e2beb4ceeb1bf48571034a08
hieuck <hieuck@users.noreply.github.com>
2023-03-31 05:52:44 +0700

    Create wget - RapidTyping.bat

---
+++ wget - RapidTyping.bat
@@ -0,0 +1,188 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+:: Set File Name Link User Agent
+set "SOFTNAME=RapidTyping"
+set "FILENAME=RapidTyping-HieuckIT.exe"
+set "PROCESS=RapidTyping.exe"
+set "LINK64=https://rapidtyping.com/en/downloads/typing-tutor/ver-5/RapidTyping_Setup_5.4.exe"
+set "LINK32=https://rapidtyping.com/en/downloads/typing-tutor/ver-5/RapidTyping_Setup_5.4_x32.exe"
+set "QUIETMODE=/S"
+set "Admin="
+set "SOFTLOCATION=%PROGRAMFILES%\RapidTyping 5\RapidTyping.exe"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Check if Command Prompt is running with administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	echo Command Prompt is running as Administrator.
+) else (
+	if "%Admin%"=="Yes" (
+		echo Please Run as Administrator. Exiting in 3 seconds...
+		for /l %%i in (3,-1,1) do (
+			echo Exiting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+		exit
+	) else (
+		echo Warning: This program may not function correctly without administrator privileges.
+		for /l %%i in (3,-1,1) do (
+			echo Starting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+	)
+)
+
+:: Terminate the %SOFTNAME% Process
+tasklist | find /i "%PROCESS%" > nul
+if %errorlevel% equ 0 (
+	taskkill /im "%PROCESS%" /f
+)
+
+:: Detect Windows Architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+	set "ARCH=x64"
+) else (
+	set "ARCH=x86"
+)
+
+:: Download
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Downloading %SOFTNAME%...
+if %ARCH%==x64 (
+	wget --no-check-certificate --show-progress -q -O "%FILENAME%" "%LINK64%"
+) else (
+	wget --no-check-certificate --show-progress -q -O "%FILENAME%" "%LINK32%"
+)
+
+if not exist "%FILENAME%" (
+	echo Download %SOFTNAME% failed.
+	echo Please check your network connection. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
+)
+
+:: Install
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Installing %SOFTNAME%...
+"%FILENAME%" %QUIETMODE%
+
+:: Check Installation Process
+if exist "%SOFTLOCATION%" (
+	echo Installation %SOFTNAME% complete.
+) else (
+	echo Installation %SOFTNAME% failed.
+	echo Please try Run as Administrator.
+)
+
+:: License
+set "License="
+set "CR4CKFILE=danvaoday"
+set "CR4CKLINK=danvaoday"
+set "SOFTPATH=danvaoday"
+if "%License%"=="Yes" (
+	echo Cracking %SOFTNAME%...
+	wget --no-check-certificate --show-progress -q -O "%CR4CKFILE%" "%CR4CKLINK%"
+	if exist "%CR4CKFILE%" (
+		move /y "%CR4CKFILE%" "%SOFTPATH%"
+	) else (
+		echo Please try running the script as Administrator.
+	)
+	if exist "%SOFTPATH%\%CR4CKFILE%" (
+		"%PROGRAMFILES%\WinRAR\UnRAR.exe" e -p123 /y "%SOFTPATH%\%CR4CKFILE%" "%SOFTPATH%"
+		echo Successfully Cr4cked %SOFTNAME%.
+		del "%SOFTPATH%\%CR4CKFILE%"
+	) else (
+		echo Cr4cking %SOFTNAME% failed.
+		echo Please try running the script as Administrator.
+	)
+)
+
+:: Shortcut
+if exist "%SOFTLOCATION%" (
+	set "TARGETFILE=%SOFTLOCATION%"
+	set "SHORTCUTNAME=RapidTyping 5.lnk"
+	set "SHORTCUTPATH=%PUBLIC%\Desktop\%SHORTCUTNAME%"
+) else (
+	echo The file "%PROCESS%" does not exist in directory "%SOFTNAME%". Exiting script.
+	exit /b 1
+)
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%SHORTCUTPATH%" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%TARGETFILE%" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
+
+if exist "%PUBLIC%\Desktop\%SHORTCUTNAME%" (
+	echo Creating shortcut complete.
+) else (
+	echo Creating shortcut failed.
+)
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 3 seconds.
+for /l %%i in (3,-1,1) do (
+	echo Closing in %%i seconds...
+	timeout /t 1 /nobreak >nul
+	if exist "%FILENAME%" (
+		tasklist | find /i "%FILENAME%" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "%FILENAME%" /f
+		)
+	del "%FILENAME%"
+	)
+)
+popd
