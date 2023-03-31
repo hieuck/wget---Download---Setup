d2c9334ab24ae04db5f813339b0842b57d663f03
hieuck <hieuck@users.noreply.github.com>
2023-03-31 13:02:25 +0700

    Update cURL - UltraViewer.bat

--- cURL - UltraViewer.bat
+++ cURL - UltraViewer.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
-mode con:cols=100 lines=20
+mode con:cols=100 lines=17
 @cls
 echo.
 echo.
@@ -11,49 +11,154 @@ echo.
 @echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
 @echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
 @echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
-@echo.
-@echo                 Dang Cai Dat UltraViewer. Vui Long Cho
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
-
 pushd "%~dp0"
-:: Terminate the UltraViewer process
-tasklist | find /i "UltraViewer_Desktop.exe" > nul
-if %errorlevel% equ 0 (
-    taskkill /im UltraViewer_Desktop.exe /f
-)
-
-:: Detect Windows architecture
-if exist "%SYSTEMROOT%\SysWOW64" (
-    set "ARCH=x64"
-) else (
-    set "ARCH=x86"
-)
-
-:: Set user agent
+:: Set File Name Link User Agent
+set "SOFTNAME=UltraViewer"
+set "FILENAME=UltraViewer-HieuckIT.exe"
+set "PROCESS=UltraViewer_Desktop.exe"
+set "LINK64=https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe"
+set "LINK32=danvaoday"
+set "QUIETMODE=/S"
+set "Admin="
+set "SOFTLOCATION=%PROGRAMFILES(x86)%\UltraViewer\"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
-:: Download
-echo Downloading UltraViewer...
-if %ARCH%==x64 (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "UltraViewer-HieuckIT.exe" "https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe"
+:: Check if Command Prompt is running with administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	echo Command Prompt is running as Administrator.
 ) else (
-    curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "UltraViewer-HieuckIT.exe" "link32"
+	if "%Admin%"=="Yes" (
+		echo Please Run as Administrator. Exiting in 3 seconds...
+		for /l %%i in (3,-1,1) do (
+			echo Exiting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+		exit
+	) else (
+		echo Warning: This program may not function correctly without administrator privileges.
+		for /l %%i in (3,-1,1) do (
+			echo Starting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+	)
+)
+
+:: Terminate the %SOFTNAME% Process
+tasklist | find /i "%PROCESS%" > nul
+if %errorlevel% equ 0 (
+	taskkill /im "%PROCESS%" /f
+)
+
+:: Detect Windows Architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+	set "ARCH=x64"
+) else (
+	set "ARCH=x86"
+)
+
+:: Download
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Downloading %SOFTNAME%...
+if %ARCH%==x64 (
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%" --insecure
+) else (
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%" --insecure
+)
+
+if not exist "%FILENAME%" (
+	echo Download %SOFTNAME% failed.
+	echo Please check your network connection. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
 )
 
 :: Install
-echo Installing UltraViewer...
-FOR %%i IN ("UltraViewer*.exe") DO Set FileName="%%i"
-%FileName% /S
-if exist "%ProgramFiles(x86)%\UltraViewer\" (
-	echo Installation UltraViewer complete.
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Installing %SOFTNAME%...
+"%FILENAME%" %QUIETMODE%
+
+:: Check Installation Process
+if exist "%SOFTLOCATION%" (
+	echo Installation %SOFTNAME% complete.
 ) else (
-	echo Installation UltraViewer failed.
+	echo Installation %SOFTNAME% failed.
 	echo Please try Run as Administrator.
 )
-::License
-::copy /y "%~dp0\banquyenneuco" "vaoday"
 
-:: Clean up
-del "UltraViewer*.exe"
-timeout /t 5
+:: License
+set "License="
+set "CR4CKFILE=danvaoday"
+set "CR4CKLINK=danvaoday"
+set "SOFTPATH=danvaoday"
+if "%License%"=="Yes" (
+	echo Cracking %SOFTNAME%...
+	wget --no-check-certificate --show-progress -q -O "%CR4CKFILE%" "%CR4CKLINK%"
+	if exist "%CR4CKFILE%" (
+		move /y "%CR4CKFILE%" "%SOFTPATH%"
+	) else (
+		echo Please try running the script as Administrator.
+	)
+	if exist "%SOFTPATH%\%CR4CKFILE%" (
+		"%PROGRAMFILES%\WinRAR\UnRAR.exe" e -p123 /y "%SOFTPATH%\%CR4CKFILE%" "%SOFTPATH%"
+		echo Successfully Cr4cked %SOFTNAME%.
+		del "%SOFTPATH%\%CR4CKFILE%"						  
+	) else (
+		echo Cr4cking %SOFTNAME% failed.
+		echo Please try running the script as Administrator.
+	)
+)
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 3 seconds.
+for /l %%i in (3,-1,1) do (
+	echo Closing in %%i seconds...
+	timeout /t 1 /nobreak >nul
+	if exist "%FILENAME%" (
+		tasklist | find /i "%FILENAME%" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "%FILENAME%" /f
+		)
+	del "%FILENAME%"
+	)
+)
 popd
