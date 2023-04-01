1ddbe648080bc4bb8f0b2e25a8b3ac94d8cc8ecb
hieuck <hieuck@users.noreply.github.com>
2023-04-01 18:20:25 +0700

    Update cURL - Screenpresso.bat

--- cURL - Screenpresso.bat
+++ cURL - Screenpresso.bat
@@ -12,15 +12,35 @@ echo.
 @echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
 @echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
 @echo.  
-@echo                 Dang Cai Dat Screenpresso. Vui Long Cho
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
 pushd "%~dp0"
-:: Set File Name Link User Agent
-set "FILENAME=Screenpresso-HieuckIT.exe"
-set "LINK64=https://www.screenpresso.com/binaries/releases/stable/dotnet47/Screenpresso.exe"
-set "LINK32=link"
-set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+:: Detect Windows Architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+	set "ARCH=x64"
+) else (
+	set "ARCH=x86"
+)
+
+:: Set Admin License Soft File Process Name User Agent
 set "Admin="
+set "License="
+set "SOFTNAME=Screenpresso"
+set "FILENAME=Screenpresso-HieuckIT.exe"
+set "PROCESS=Screenpresso.exe"&set "PROCESS=ScreenpressoRpc.exe"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Set code based on Windows Architecture
+if %ARCH%==x64 (
+	set "LINK=https://www.screenpresso.com/binaries/releases/stable/dotnet47/Screenpresso.exe"
+) else (
+	set "LINK=https://www.screenpresso.com/download/old-versions/Screenpresso-1.7.8.0-32-bit.exe"
+)
+set "QUIETMODE=deploy --install --programfiles --quiet"
+set "CR4CKFILE=danvaoday.rar"
+set "CR4CKLINK=danvaoday"
+set "SOFTPATH=%PROGRAMFILES%\Learnpulse\Screenpresso"
+set "SOFTLOCATION=%SOFTPATH%\%PROCESS%"
 
 :: Check if Command Prompt is running with administrator privileges
 net session >nul 2>&1
@@ -28,36 +48,25 @@ if %errorlevel% == 0 (
 	echo Command Prompt is running as Administrator.
 ) else (
 	if "%Admin%"=="Yes" (
-		echo Please Run as Administrator. Exiting in 5 seconds...
-		for /l %%i in (5,-1,1) do (
+		echo Please Run as Administrator. Exiting in 3 seconds...
+		for /l %%i in (3,-1,1) do (
 			echo Exiting in %%i seconds...
 			timeout /t 1 /nobreak >nul
 		)
 		exit
 	) else (
 		echo Warning: This program may not function correctly without administrator privileges.
-		for /l %%i in (5,-1,1) do (
+		for /l %%i in (3,-1,1) do (
 			echo Starting in %%i seconds...
 			timeout /t 1 /nobreak >nul
 		)
 	)
 )
 
-:: Terminate the Screenpresso Process
-tasklist | find /i "Screenpresso.exe" > nul
+:: Terminate the %SOFTNAME% Process
+tasklist | find /i "%PROCESS%" > nul
 if %errorlevel% equ 0 (
-	taskkill /im "Screenpresso.exe" /f
-)
-tasklist | find /i "ScreenpressoRpc.exe" > nul
-if %errorlevel% equ 0 (
-	taskkill /im "ScreenpressoRpc.exe" /f
-)
-
-:: Detect Windows Architecture
-if exist "%SYSTEMROOT%\SysWOW64" (
-	set "ARCH=x64"
-) else (
-	set "ARCH=x86"
+	taskkill /im "%PROCESS%" /f
 )
 
 :: Download
@@ -75,20 +84,16 @@ echo.
 @echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
 @echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
 @echo.  
-@echo                 Dang Cai Dat Screenpresso. Vui Long Cho
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
 pushd "%~dp0"
-echo Downloading Screenpresso...
-if %ARCH%==x64 (
-	curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK64%"
-) else (
-	curl --insecure -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK32%"
-)
+echo Downloading %SOFTNAME%...
+curl -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK%" --insecure
 
 if not exist "%FILENAME%" (
-	echo Download Screenpresso failed.
-	echo Please check your network connection. Exiting in 5 seconds...
-	for /l %%i in (5,-1,1) do (
+	echo Download %SOFTNAME% failed.
+	echo Please check your network connection. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
 		echo Exiting in %%i seconds...
 		timeout /t 1 /nobreak >nul
 	)
@@ -110,60 +115,32 @@ echo.
 @echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
 @echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
 @echo.  
-@echo                 Dang Cai Dat Screenpresso. Vui Long Cho
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
 pushd "%~dp0"
-echo Installing Screenpresso...
-"%FILENAME%" deploy --install --programfiles --quiet
+echo Installing %SOFTNAME%...
+"%FILENAME%" %QUIETMODE%
 
 :: Check Installation Process
-if not exist "%ProgramFiles%\Learnpulse\Screenpresso\Screenpresso.exe" (
-	echo Installation Screenpresso failed.
-	echo Please try Run as Administrator. Exiting in 5 seconds...
-	for /l %%i in (5,-1,1) do (
-		echo Exiting in %%i seconds...
-		timeout /t 1 /nobreak >nul
-	)
-	exit
+if exist "%SOFTLOCATION%" (
+	echo Installation %SOFTNAME% complete.
 ) else (
-	echo Installation Screenpresso complete.
+	echo Installation %SOFTNAME% failed.
+	echo Please try Run as Administrator.
 )
 
 :: License
-echo Cr4cking Screenpresso...
 echo Please exit Screenpresso when the 'Operation succeeded' command below appears
 "%ProgramFiles%\Learnpulse\Screenpresso\Screenpresso.exe" license --activate [3]-[screenpressopro]-[1314]-[Meffi/tPORt]-[11/10/2022]-[CCmBVJV+jaQzzj6K1OypBEp0a4JLoGunMBnIZRsEKNau6wDIOaYGz6pG81MT6JJSeOS/OIdBsMBMzCBHrDBHgQ==] --quiet
 netsh advfirewall firewall show rule name="Block Screenpresso" > nul
 if %errorlevel% neq 0 (
     netsh advfirewall firewall add rule name="Block Screenpresso" dir=out action=block program="%ProgramFiles%\Learnpulse\Screenpresso\Screenpresso.exe" enable=yes
 )
-tasklist | find /i "Screenpresso.exe" > nul
-if %errorlevel% equ 0 (
-	echo Cr4ck Screenpresso failed.
-	echo Please try Run as Administrator.
-)
 
 :: Clean Up
-@ECHO OFF
-title _Hieuck.IT_'s Windows Application
-color 0B
-mode con:cols=100 lines=17
-@cls
-echo.
-echo.
-echo.
-@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
-@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
-@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
-@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
-@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
-@echo.  
-@echo                 Dang Cai Dat Screenpresso. Vui Long Cho
-@echo off
-pushd "%~dp0"
 del "%FILENAME%"
-echo The script will automatically close in 5 seconds.
-for /l %%i in (5,-1,1) do (
+echo The script will automatically close in 3 seconds.
+for /l %%i in (3,-1,1) do (
 	echo Closing in %%i seconds...
 	timeout /t 1 /nobreak >nul
 	if exist "%FILENAME%" (
