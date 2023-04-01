55c0af0f7d424d9caf7b06e425f3a0be9601dc21
hieuck <hieuck@users.noreply.github.com>
2023-04-01 19:38:26 +0700

    Update cURL - Bitwarden.bat

--- cURL - Bitwarden.bat
+++ cURL - Bitwarden.bat
@@ -146,7 +146,7 @@ if "%License%"=="Yes" (
 	if exist "%SOFTPATH%\%CR4CKFILE%" (
 		"%PROGRAMFILES%\WinRAR\UnRAR.exe" e -p123 /y "%SOFTPATH%\%CR4CKFILE%" "%SOFTPATH%"
 		echo Successfully Cr4cked %SOFTNAME%.
-		del "%SOFTPATH%\%CR4CKFILE%"						  
+		del "%SOFTPATH%\%CR4CKFILE%"
 	) else (
 		echo Cr4cking %SOFTNAME% failed.
 		echo Please try running the script as Administrator.
