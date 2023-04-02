0538a8368418166b955da7f37ea8279b2d87422e
hieuck <hieuck@users.noreply.github.com>
2023-04-02 18:09:47 +0700

    Update cURL - MẫuFinal .bat

--- cURL - MẫuFinal .bat
+++ cURL - MẫuFinal .bat
@@ -31,7 +31,7 @@ set "PROCESS=danvaoday.exe"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 :: Set code based on Windows Architecture
-if %ARCH%==X86 (
+if %ARCH%==x86 (
 	set "LINK=danvaoday"
 	set "QUIETMODE=danvaoday"
 	set "CR4CKFILE=danvaoday.rar"
