258c1981efb55f23e84d9efbe86a556f3c6872dc
hieuck <hieuck@users.noreply.github.com>
2023-04-04 17:21:25 +0700

    Update cURL - K-Lite.bat

--- cURL - K-Lite.bat
+++ cURL - K-Lite.bat
@@ -122,12 +122,21 @@ echo Installing %SOFTNAME%...
 "%FILENAME%" %QUIETMODE%
 
 :: Check Installation Process
-if exist "%SOFTLOCATION%" (
-	echo Installation %SOFTNAME% complete.
-) else (
-	echo Installation %SOFTNAME% failed.
-	echo Please try Run as Administrator.
-)
+echo Checking if %SOFTNAME% installation is complete...
+setlocal EnableDelayedExpansion
+set count=0
+:waitloop
+timeout /t 1 /nobreak > nul
+set /a count+=1
+if exist "%SOFTLOCATION%" goto installed
+if !count! equ 30 goto timeout
+goto waitloop
+:timeout
+echo Timeout: %SOFTNAME% installation has not completed in 30 seconds.
+goto end
+:installed
+echo %SOFTNAME% has been installed successfully!
+:end
 
 :: License
 if "%License%"=="Yes" (
