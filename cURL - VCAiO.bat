a85c0e8be9d3c5d3c0ce25b259a8844b77a28dcc
hieuck <hieuck@users.noreply.github.com>
2023-04-06 12:50:45 +0700

    Update cURL - VCAiO.bat

--- cURL - VCAiO.bat
+++ cURL - VCAiO.bat
@@ -22,51 +22,28 @@ if exist "%SYSTEMROOT%\SysWOW64" (
 	set "ARCH=x86"
 )
 
-:: Set Admin Name URL User Agent
-set "Admin="
-set "SOFTNAME=Visual C++ runtime"
-set "url2005=https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.EXE"
-set "url2005x64=https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.EXE"
-set "url2008=https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x86.exe"
-set "url2008x64=https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x64.exe"
-set "url2010=https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe"
-set "url2010x64=https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe"
-set "url2012=https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe"
-set "url2012x64=https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe"
-set "url2013=https://download.visualstudio.microsoft.com/download/pr/10912113/5da66ddebb0ad32ebd4b922fd82e8e25/vcredist_x86.exe"
-set "url2013x64=https://download.visualstudio.microsoft.com/download/pr/10912041/cee5d6bca2ddbcd039da727bf4acb48a/vcredist_x64.exe"
-set "url2015=https://download.visualstudio.microsoft.com/download/pr/6a4c74cd-8ee0-4757-9620-a11a5b48b1a7/CE4843A946EE3732EB2BFC098DB5741DC5495C7BEA204E11D379336DCC68E875/VC_redist.x86.exe"
-set "url2015x64=https://download.visualstudio.microsoft.com/download/pr/bcb0cef1-f8cb-4311-8a5c-650a5b694eab/2257B3FBE3C7559DE8B31170155A433FAF5B83829E67C589D5674FF086B868B9/VC_redist.x64.exe"
+:: Set Name User Agent
+set "SOFTNAME=Microsoft Visual C++ runtimes"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-set version=%1
-set url=url%version%
-set url=!%url%!
-set installer=vcredist_%version%_%arch%.exe
-set downloadpath=%~dp0\vc_redist
 
-:: Create download directory
-if not exist "%downloadpath%" mkdir "%downloadpath%"
+:: Set code based on Windows Architecture
+setlocal EnableDelayedExpansion
+set "vc2005=https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.EXE"
+set "vc2008=https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x86.exe"
+set "vc2010=https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe"
+set "vc2012=https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe"
+set "vc2013=https://download.visualstudio.microsoft.com/download/pr/10912113/5da66ddebb0ad32ebd4b922fd82e8e25/vcredist_x86.exe"
+set "vc2015=https://download.visualstudio.microsoft.com/download/pr/6a4c74cd-8ee0-4757-9620-a11a5b48b1a7/CE4843A946EE3732EB2BFC098DB5741DC5495C7BEA204E11D379336DCC68E875/VC_redist.x86.exe"
+set "vc2005x64=https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.EXE"
+set "vc2008x64=https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x64.exe"
+set "vc2010x64=https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe"
+set "vc2012x64=https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe"
+set "vc2013x64=https://download.visualstudio.microsoft.com/download/pr/10912041/cee5d6bca2ddbcd039da727bf4acb48a/vcredist_x64.exe"
+set "vc2015x64=https://download.visualstudio.microsoft.com/download/pr/bcb0cef1-f8cb-4311-8a5c-650a5b694eab/2257B3FBE3C7559DE8B31170155A433FAF5B83829E67C589D5674FF086B868B9/VC_redist.x64.exe"
 
-:: Check if Command Prompt is running with administrator privileges
-net session >nul 2>&1
-if %errorlevel% == 0 (
-	echo Command Prompt is running as Administrator.
-) else (
-	if "%Admin%"=="Yes" (
-		echo Please Run as Administrator. Exiting in 3 seconds...
-		for /l %%i in (3,-1,1) do (
-			echo Exiting in %%i seconds...
-			timeout /t 1 /nobreak >nul
-		)
-		exit
-	) else (
-		echo Warning: This program may not function correctly without administrator privileges.
-		for /l %%i in (3,-1,1) do (
-			echo Starting in %%i seconds...
-			timeout /t 1 /nobreak >nul
-		)
-	)
-)
+:: Create download folder
+set "download_folder=%~dp0\vc_redist"
+md "%download_folder%" 2>NUL
 
 :: Download
 @ECHO OFF
@@ -87,14 +64,30 @@ echo.
 @echo off
 pushd "%~dp0"
 echo Downloading %SOFTNAME%...
-curl -L --max-redirs 20 -A "%USERAGENT%" -o "%downloadpath%\%installer%" "%url%" --insecure
-if exist "%downloadpath%\%installer%" (
-    echo Download completed. Starting installation...
+
+if "%arch%"=="x64" (
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2005_x86.exe" "%vc2005%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2008_x86.exe" "%vc2008%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2010_x86.exe" "%vc2010%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2012_x86.exe" "%vc2012%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2013_x86.exe" "%vc2013%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2015_x86.exe" "%vc2015%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2005_x64.exe" "%vc2005x64%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2008_x64.exe" "%vc2008x64%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2010_x64.exe" "%vc2010x64%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2012_x64.exe" "%vc2012x64%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2013_x64.exe" "%vc2013x64%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2015_x64.exe" "%vc2015x64%" --insecure
 ) else (
-    echo Download failed. Installation aborted.
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2005_x86.exe" "%vc2005%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2008_x86.exe" "%vc2008%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2010_x86.exe" "%vc2010%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2012_x86.exe" "%vc2012%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2013_x86.exe" "%vc2013%" --insecure
+    curl -L --max-redirs 20 -A "%USERAGENT%" -o "%download_folder%\vc2015_x86.exe" "%vc2015%" --insecure
 )
 
-::Installer
+:: Install
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
@@ -112,25 +105,49 @@ echo.
 @echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
 pushd "%~dp0"
-if "%arch%"=="x86" (
-    start /wait "" "%downloadpath%\%installer%" /q /norestart
+echo Installing %SOFTNAME%...
+if "%arch%"=="x64" (
+	start /wait "" "%download_folder%\vc2005_x86.exe" /q
+	start /wait "" "%download_folder%\vc2008_x86.exe" /q
+	start /wait "" "%download_folder%\vc2010_x86.exe" /q
+	start /wait "" "%download_folder%\vc2012_x86.exe" /q
+	start /wait "" "%download_folder%\vc2013_x86.exe" /q
+	start /wait "" "%download_folder%\vc2015_x86.exe" /install /quiet /norestart
+	start /wait "" "%download_folder%\vc2005_x64.exe" /q
+	start /wait "" "%download_folder%\vc2008_x64.exe" /q
+	start /wait "" "%download_folder%\vc2010_x64.exe" /q
+	start /wait "" "%download_folder%\vc2012_x64.exe" /q
+	start /wait "" "%download_folder%\vc2013_x64.exe" /q
+	start /wait "" "%download_folder%\vc2015_x64.exe" /install /quiet /norestart
 ) else (
-    start /wait "" "%downloadpath%\%installer%" /q /norestart /install
-    start /wait "" "%downloadpath%\%installer%" /q /norestart /install /passive
+	start /wait "" "%download_folder%\vc2005_x86.exe" /q
+	start /wait "" "%download_folder%\vc2008_x86.exe" /q
+	start /wait "" "%download_folder%\vc2010_x86.exe" /q
+	start /wait "" "%download_folder%\vc2012_x86.exe" /q
+	start /wait "" "%download_folder%\vc2013_x86.exe" /q
+	start /wait "" "%download_folder%\vc2015_x86.exe" /install /quiet /norestart
 )
 
-:: Clean Up
-del "%downloadpath%\%installer%"
+:: clean up
+echo Cleaning up temporary files...
+if exist "%download_folder%\vc2005_x86.exe" del "%download_folder%\vc2005_x86.exe"
+if exist "%download_folder%\vc2005_x64.exe" del "%download_folder%\vc2005_x64.exe"
+if exist "%download_folder%\vc2008_x86.exe" del "%download_folder%\vc2008_x86.exe"
+if exist "%download_folder%\vc2008_x64.exe" del "%download_folder%\vc2008_x64.exe"
+if exist "%download_folder%\vc2010_x86.exe" del "%download_folder%\vc2010_x86.exe"
+if exist "%download_folder%\vc2010_x64.exe" del "%download_folder%\vc2010_x64.exe"
+if exist "%download_folder%\vc2012_x86.exe" del "%download_folder%\vc2012_x86.exe"
+if exist "%download_folder%\vc2012_x64.exe" del "%download_folder%\vc2012_x64.exe"
+if exist "%download_folder%\vc2013_x86.exe" del "%download_folder%\vc2013_x86.exe"
+if exist "%download_folder%\vc2013_x64.exe" del "%download_folder%\vc2013_x64.exe"
+if exist "%download_folder%\vc2015_x86.exe" del "%download_folder%\vc2015_x86.exe"
+if exist "%download_folder%\vc2015_x64.exe" del "%download_folder%\vc2015_x64.exe"
+rmdir /s /q "%download_folder%"
+
 echo The script will automatically close in 3 seconds.
 for /l %%i in (3,-1,1) do (
 	echo Closing in %%i seconds...
 	timeout /t 1 /nobreak >nul
-	if exist "%downloadpath%\%installer%" (
-		tasklist | find /i "%downloadpath%\%installer%" > nul
-		if %errorlevel% equ 0 (
-			taskkill /im "%downloadpath%\%installer%" /f
-		)
-	del "%downloadpath%\%installer%"
 	)
 )
 popd
