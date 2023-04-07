12b543a4591b350272b594fd076454b1b2997543
hieuck <hieuck@users.noreply.github.com>
2023-04-07 19:35:48 +0700

    Create cURL - TeraCopy.bat

---
+++ cURL - TeraCopy.bat
@@ -0,0 +1,161 @@
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+:: Detect Windows Architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+	set "ARCH=x64"
+) else (
+	set "ARCH=x86"
+)
+
+:: Set Admin License Soft File Process Name User Agent
+set "Admin="
+set "License="
+set "SOFTNAME=TeraCopy"
+set "FILENAME=TeraCopy-HieuckIT.exe"
+set "PROCESS=TeraCopy.exe"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Set code based on Windows Architecture
+set "LINK=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/TeraCopyCr4ck/TeraCopy.Pro.v3.6.0.4.exe"
+set "QUIETMODE=/S"
+set "CR4CKFILE=danvaoday.rar"
+set "CR4CKLINK=danvaoday"
+set "SOFTPATH=%PROGRAMFILES%\TeraCopy"
+set "SOFTLOCATION=%SOFTPATH%\%PROCESS%"
+
+:: Check if Command Prompt is running with administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	echo Command Prompt is running as Administrator.
+) else (
+	if "%Admin%"=="Yes" (
+		echo Please Run as Administrator. Exiting in 3 seconds...
+		for /l %%i in (3,-1,1) do (
+			echo Exiting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+		exit
+	) else (
+		echo Warning: This program may not function correctly without administrator privileges.
+		for /l %%i in (3,-1,1) do (
+			echo Starting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+	)
+)
+
+:: Terminate the %SOFTNAME% Process
+tasklist | find /i "%PROCESS%" > nul
+if %errorlevel% equ 0 (
+	taskkill /im "%PROCESS%" /f
+)
+
+:: Download
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Downloading %SOFTNAME%...
+curl -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK%" --insecure
+
+if not exist "%FILENAME%" (
+	echo Download %SOFTNAME% failed.
+	echo Please check your network connection. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
+)
+
+:: Install
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Installing %SOFTNAME%...
+"%FILENAME%" %QUIETMODE%
+
+:: Check Installation Process
+if exist "%SOFTLOCATION%" (
+	echo Installation %SOFTNAME% complete.
+) else (
+	echo Installation %SOFTNAME% failed.
+	echo Please try Run as Administrator.
+)
+
+:: License
+if "%License%"=="Yes" (
+	echo Cr4cking %SOFTNAME%...
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "%CR4CKFILE%" "%CR4CKLINK%" --insecure
+	if exist "%CR4CKFILE%" (
+		move /y "%CR4CKFILE%" "%SOFTPATH%"
+	) else (
+		echo Please try running the script as Administrator.
+	)
+	if exist "%SOFTPATH%\%CR4CKFILE%" (
+		"%PROGRAMFILES%\WinRAR\UnRAR.exe" e -p123 /y "%SOFTPATH%\%CR4CKFILE%" "%SOFTPATH%"
+		echo Successfully Cr4cked %SOFTNAME%.
+		del "%SOFTPATH%\%CR4CKFILE%"
+	) else (
+		echo Cr4cking %SOFTNAME% failed.
+		echo Please try running the script as Administrator.
+	)
+)
+
+:: Clean Up
+del "%FILENAME%"
+echo The script will automatically close in 3 seconds.
+for /l %%i in (3,-1,1) do (
+	echo Closing in %%i seconds...
+	timeout /t 1 /nobreak >nul
+	if exist "%FILENAME%" (
+		tasklist | find /i "%FILENAME%" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "%FILENAME%" /f
+		)
+	del "%FILENAME%"
+	)
+)
+popd
