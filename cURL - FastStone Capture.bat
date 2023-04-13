1b836ae04b45c82055ba074f7ef2b2891382be1a
hieuck <hieuck@users.noreply.github.com>
2023-04-13 20:46:38 +0700

    Update cURL - FastStone Capture.bat

--- cURL - FastStone Capture.bat
+++ cURL - FastStone Capture.bat
@@ -26,7 +26,6 @@ if exist "%SYSTEMROOT%\SysWOW64" (
 set "License=Yes"
 set "Extract7z="
 set "SOFTNAME=FastStone Capture"
-set "FILENAME=%SOFTNAME%-HieuckIT.exe"
 set "PROCESS=FSCapture.exe"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
@@ -43,7 +42,7 @@ set "QUIETMODE=/S"
 if "%License%"=="Yes" (
 	set "Admin=Yes"
 	set "CR4CKFILE=FastStoneCaptureCr4ck.rar"
-	set "CR4CKLINK=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/FastStoneCr4ck/FastStoneCaptureCr4ck.rar"
+	set "CR4CKLINK=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/Cr4ck/FastStoneCaptureCr4ck.rar"
 	set "CR4CKPATH=%LOCALAPPDATA%\FastStone\FSC"
 	set "LINK7zdll=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.dll"
 	set "LINK7zexe=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.exe"
@@ -51,6 +50,7 @@ if "%License%"=="Yes" (
 
 ::Extract with 7z
 if "%Extract7z%"=="Yes" (
+	set "FILENAME=%SOFTNAME%-HieuckIT.zip"
 	set "Admin=Yes"
 	set "Shortcut=Yes"
 	set "SOFTPATH=%PROGRAMFILES%\%SOFTNAME%"
@@ -58,6 +58,7 @@ if "%Extract7z%"=="Yes" (
 	set "LINK7zexe=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.exe"
 ) else (
 	set "Shortcut=No"
+	set "FILENAME=%SOFTNAME%-HieuckIT.exe"
 )
 set "SOFTLOCATION=%SOFTPATH%\%PROCESS%"
 
@@ -136,7 +137,7 @@ echo.
 @echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
 @echo off
 pushd "%~dp0"
-echo Downloading 7z...
+echo Downloading 7-Zip...
 if "%License%"=="Yes" (
 	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.dll" "%LINK7zdll%" --insecure
 	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.exe" "%LINK7zexe%" --insecure
@@ -170,13 +171,6 @@ if "%Extract7z%"=="Yes" (
 	"%FILENAME%" %QUIETMODE%
 )
 
-:: Check Installation Process
-if exist "%SOFTLOCATION%" (
-	echo Installation %SOFTNAME% complete.
-) else (
-	echo Installation %SOFTNAME% failed.
-	echo Please try Run as Administrator.
-)
 :: Check Installation Process
 echo Checking if %SOFTNAME% installation is complete...
 setlocal EnableDelayedExpansion
@@ -242,14 +236,33 @@ if exist "%PUBLIC%\Desktop\%SHORTCUTNAME%" (
 
 :: Clean Up
 :CleanUp
-del 7z.dll
-del 7z.exe
 del "%FILENAME%"
+if "%License%"=="Yes" (
+	del 7z.dll
+	del 7z.exe
+) else if "%Extract7z%"=="Yes" (
+	del 7z.dll
+	del 7z.exe
+)
 
 echo The script will automatically close in 3 seconds.
 for /l %%i in (3,-1,1) do (
 	echo Closing in %%i seconds...
 	timeout /t 1 /nobreak >nul
+	if exist "7z.dll" (
+		tasklist | find /i "7z.dll" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "7z.dll" /f
+		)
+	del "7z.dll"
+	)
+	if exist "7z.exe" (
+		tasklist | find /i "7z.exe" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "7z.exe" /f
+		)
+	del "7z.exe"
+	)
 	if exist "%FILENAME%" (
 		tasklist | find /i "%FILENAME%" > nul
 		if %errorlevel% equ 0 (
