85af38482973be412bb8eaacc22cc667632211c5
hieuck <hieuck@users.noreply.github.com>
2023-04-15 09:04:01 +0700

    Update AnyDesk.bat

--- AnyDesk/AnyDesk.bat
+++ AnyDesk/AnyDesk.bat
@@ -16,15 +16,15 @@ echo.
 if exist %Windir%\SysWOW64 goto X64
 
 FOR %%i IN ("AnyDesk*.exe") DO Set FileName="%%i"
-%FileName% --install "C:\Program Files\AnyDesk" --start-with-win --create-shortcuts --create-desktop-icon --silent
-
+%FileName% --install "C:\Program Files\AnyDesk" --start-with-win --create-shortcuts --create-desktop-icon --update-disabled --silent
+echo hieuckit | "C:\Program Files\AnyDesk\AnyDesk.exe" --set-password
 goto END
 
 :X64
 
 FOR %%i IN ("AnyDesk*.exe") DO Set FileName="%%i"
-%FileName% --install "C:\Program Files (x86)\AnyDesk" --start-with-win --create-shortcuts --create-desktop-icon --silent
-
+%FileName% --install "C:\Program Files (x86)\AnyDesk" --start-with-win --create-shortcuts --create-desktop-icon --update-disabled --silent
+echo hieuckit | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password
 goto END
 
 :END
