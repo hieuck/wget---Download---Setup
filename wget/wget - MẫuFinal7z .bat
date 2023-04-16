4ac1b7d7e0eb63d485719edc867d6b897f47350a
hieuck <hieuck@users.noreply.github.com>
2023-04-16 10:21:59 +0700

    Final Code

--- wget/wget - MẫuFinal7z .bat
+++ wget/wget - MẫuFinal7z .bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
-mode con:cols=100 lines=17
+mode con:cols=100 lines=15
 @cls
 echo.
 echo.
@@ -103,7 +103,7 @@ if %errorlevel% equ 0 (
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
-mode con:cols=100 lines=17
+mode con:cols=100 lines=15
 @cls
 echo.
 echo.
@@ -118,7 +118,7 @@ echo.
 @echo off
 pushd "%~dp0"
 echo Downloading %SOFTNAME%...
-curl -L --max-redirs 20 -A "%USERAGENT%" -o "%FILENAME%" "%LINK%" --insecure
+wget --no-check-certificate --show-progress -q -O "%FILENAME%" "%LINK%"
 
 if not exist "%FILENAME%" (
 	echo Download %SOFTNAME% failed.
@@ -133,7 +133,7 @@ if not exist "%FILENAME%" (
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
-mode con:cols=100 lines=17
+mode con:cols=100 lines=15
 @cls
 echo.
 echo.
@@ -149,15 +149,15 @@ echo.
 pushd "%~dp0"
 echo Downloading 7-Zip...
 if "%Extract7z%"=="Yes" (
-	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.dll" "%LINK7zdll%" --insecure
-	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.exe" "%LINK7zexe%" --insecure
+	wget --no-check-certificate --show-progress -q -O "7z.dll" "%LINK7zdll%"
+	wget --no-check-certificate --show-progress -q -O "7z.exe" "%LINK7zexe%"
 )
 
 :: Install
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
-mode con:cols=100 lines=17
+mode con:cols=100 lines=15
 @cls
 echo.
 echo.
@@ -199,7 +199,7 @@ echo %SOFTNAME% has been installed successfully!
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
 color 0B
-mode con:cols=100 lines=17
+mode con:cols=100 lines=15
 @cls
 echo.
 echo.
@@ -215,9 +215,9 @@ echo.
 pushd "%~dp0"
 if "%License%"=="Yes" (
 	echo Cr4cking %SOFTNAME%...
-	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.dll" "%LINK7zdll%" --insecure
-	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.exe" "%LINK7zexe%" --insecure
-	curl -L --max-redirs 20 -A "%USERAGENT%" -o "%CR4CKFILE%" "%CR4CKLINK%" --insecure
+	wget --no-check-certificate --show-progress -q -O "7z.dll" "%LINK7zdll%"
+	wget --no-check-certificate --show-progress -q -O "7z.exe" "%LINK7zexe%"
+	wget --no-check-certificate --show-progress -q -O "%CR4CKFILE%" "%CR4CKLINK%"
 	if exist "%CR4CKFILE%" (
 		@7z.exe x -p123 "%CR4CKFILE%" -o"%CR4CKPATH%" -aoa -y
 		echo Successfully Cr4cked %SOFTNAME%.
