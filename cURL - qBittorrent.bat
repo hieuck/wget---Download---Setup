eb0532addd64caf37c1d29bf6ce26056b0bd2c5e
hieuck <hieuck@users.noreply.github.com>
2023-04-17 12:27:58 +0700

    Update cURL - qBittorrent.bat

--- cURL - qBittorrent.bat
+++ cURL - qBittorrent.bat
@@ -22,11 +22,10 @@ if exist "%SYSTEMROOT%\SysWOW64" (
 	set "ARCH=x86"
 )
 
-:: Set Admin License Soft File Process Name User Agent
-set "Admin="
+:: Set License Extract7z Soft Process Name User Agent
 set "License="
+set "Extract7z="
 set "SOFTNAME=qBittorrent"
-set "FILENAME=qBittorrent-HieuckIT.exe"
 set "PROCESS=qBittorrent.exe"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
@@ -41,9 +40,30 @@ if %ARCH%==x86 (
 )
 set "LINK=https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.5.2_lt20_qt6_x64_setup.exe"
 set "QUIETMODE=/S"
-set "CR4CKFILE=danvaoday.rar"
-set "CR4CKLINK=danvaoday"
 set "SOFTPATH=%PROGRAMFILES%\qBittorrent"
+
+:: Set up information related to software cr4cking
+if "%License%"=="Yes" (
+	set "Admin=Yes"
+	set "CR4CKFILE=danvaoday"
+	set "CR4CKPATH=%SOFTPATH%"
+	set "CR4CKLINK=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/Cr4ck/!CR4CKFILE!.rar"
+	set "LINK7zdll=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.dll"
+	set "LINK7zexe=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.exe"
+)
+
+::Extract with 7z
+if "%Extract7z%"=="Yes" (
+	set "FILENAME=%SOFTNAME%-HieuckIT.zip"
+	set "Admin=Yes"
+	set "Shortcut=Yes"
+	set "SOFTPATH=%PROGRAMFILES%\%SOFTNAME%"
+	set "LINK7zdll=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.dll"
+	set "LINK7zexe=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.exe"
+) else (
+	set "Shortcut=No"
+	set "FILENAME=%SOFTNAME%-HieuckIT.exe"
+)
 set "SOFTLOCATION=%SOFTPATH%\%PROCESS%"
 
 :: Check if Command Prompt is running with administrator privileges
@@ -104,6 +124,29 @@ if not exist "%FILENAME%" (
 	exit
 )
 
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
+echo Downloading 7-Zip...
+if "%Extract7z%"=="Yes" (
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.dll" "%LINK7zdll%" --insecure
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.exe" "%LINK7zexe%" --insecure
+)
+
 :: Install
 @ECHO OFF
 title _Hieuck.IT_'s Windows Application
@@ -123,41 +166,123 @@ echo.
 @echo off
 pushd "%~dp0"
 echo Installing %SOFTNAME%...
-"%FILENAME%" %QUIETMODE%
-
-:: Check Installation Process
-if exist "%SOFTLOCATION%" (
-	echo Installation %SOFTNAME% complete.
+if "%Extract7z%"=="Yes" (
+	@7z.exe x "%FILENAME%" -o"%SOFTPATH%" -aoa -y
 ) else (
-	echo Installation %SOFTNAME% failed.
-	echo Please try Run as Administrator.
+	"%FILENAME%" %QUIETMODE%
 )
 
+:: Check Installation Process
+echo Checking if %SOFTNAME% installation is complete...
+setlocal EnableDelayedExpansion
+set count=0
+:waitloop
+timeout /t 1 /nobreak > nul
+set /a count+=1
+if exist "%SOFTLOCATION%" goto installed
+if !count! equ 30 goto timeout
+goto waitloop
+:timeout
+echo Timeout: %SOFTNAME% installation has not completed in 30 seconds.
+goto end
+:installed
+echo %SOFTNAME% has been installed successfully!
+:end
+
 :: License
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.  
+@echo                 Dang Cai Dat %SOFTNAME%. Vui Long Cho
+@echo off
+pushd "%~dp0"
 if "%License%"=="Yes" (
 	echo Cr4cking %SOFTNAME%...
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.dll" "%LINK7zdll%" --insecure
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.exe" "%LINK7zexe%" --insecure
 	curl -L --max-redirs 20 -A "%USERAGENT%" -o "%CR4CKFILE%" "%CR4CKLINK%" --insecure
 	if exist "%CR4CKFILE%" (
-		move /y "%CR4CKFILE%" "%SOFTPATH%"
-	) else (
-		echo Please try running the script as Administrator.
-	)
-	if exist "%SOFTPATH%\%CR4CKFILE%" (
-		"%PROGRAMFILES%\WinRAR\UnRAR.exe" e -p123 /y "%SOFTPATH%\%CR4CKFILE%" "%SOFTPATH%"
+		@7z.exe x -p123 "%CR4CKFILE%" -o"%CR4CKPATH%" -aoa -y
 		echo Successfully Cr4cked %SOFTNAME%.
-		del "%SOFTPATH%\%CR4CKFILE%"
+		del "%CR4CKFILE%"
 	) else (
 		echo Cr4cking %SOFTNAME% failed.
 		echo Please try running the script as Administrator.
 	)
 )
 
+:: Shortcut
+if /i "%Shortcut%"=="no" (
+    echo Creating shortcut is skipped.
+    goto CleanUp
+)
+
+if exist "%SOFTLOCATION%" (
+	set "TARGETFILE=%SOFTLOCATION%"
+) else (
+	echo %SOFTNAME% does not exist in directory "%SOFTPATH%". Exiting script.
+	exit /b 1
+)
+
+set "SHORTCUTNAME=%SOFTNAME%.lnk"
+set "SHORTCUTPATH=%PUBLIC%\Desktop\%SHORTCUTNAME%"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%SHORTCUTPATH%" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%TARGETFILE%" >> CreateShortcut.vbs
+echo oLink.WorkingDirectory = "%SOFTPATH%" >> CreateShortcut.vbs
+echo oLink.Description = "Shortcut to %SOFTNAME%" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
+
+if exist "%PUBLIC%\Desktop\%SHORTCUTNAME%" (
+	echo Creating shortcut complete.
+) else (
+	echo Creating shortcut failed.
+)
+
 :: Clean Up
+:CleanUp
 del "%FILENAME%"
+if "%License%"=="Yes" (
+	del 7z.dll
+	del 7z.exe
+) else if "%Extract7z%"=="Yes" (
+	del 7z.dll
+	del 7z.exe
+)
+
 echo The script will automatically close in 3 seconds.
 for /l %%i in (3,-1,1) do (
 	echo Closing in %%i seconds...
 	timeout /t 1 /nobreak >nul
+	if exist "7z.dll" (
+		tasklist | find /i "7z.dll" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "7z.dll" /f
+		)
+	del "7z.dll"
+	)
+	if exist "7z.exe" (
+		tasklist | find /i "7z.exe" > nul
+		if %errorlevel% equ 0 (
+			taskkill /im "7z.exe" /f
+		)
+	del "7z.exe"
+	)
 	if exist "%FILENAME%" (
 		tasklist | find /i "%FILENAME%" > nul
 		if %errorlevel% equ 0 (
