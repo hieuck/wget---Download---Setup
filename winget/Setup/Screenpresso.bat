59810b7917a8c6bdcaf07b2b8e1dd23b34188ed3
hieuck <hieuck@users.noreply.github.com>
2023-04-17 07:10:53 +0700

    Update Screenpresso.bat

--- winget/Setup/Screenpresso.bat
+++ winget/Setup/Screenpresso.bat
@@ -15,3 +15,11 @@ echo.
 @echo                 Dang Cai Dat Screenpresso Tu Dong. Vui Long Cho
 @echo off
 winget install -e --id Learnpulse.Screenpresso
+
+set "SOFTLOCATION=%PROGRAMFILES%\Learnpulse\Screenpresso\Screenpresso.exe"
+echo Please exit Screenpresso when the 'Operation succeeded' command below appears
+netsh advfirewall firewall show rule name="Block Screenpresso" > nul
+if %errorlevel% neq 0 (
+    netsh advfirewall firewall add rule name="Block Screenpresso" dir=out action=block program="%ProgramFiles%\Learnpulse\Screenpresso\Screenpresso.exe" enable=yes
+)
+"%SOFTLOCATION%" license --activate [3]-[screenpressopro]-[1314]-[Meffi/tPORt]-[11/10/2022]-[CCmBVJV+jaQzzj6K1OypBEp0a4JLoGunMBnIZRsEKNau6wDIOaYGz6pG81MT6JJSeOS/OIdBsMBMzCBHrDBHgQ==] --quiet
