502b4ddde3e793c3f9024b04c2d7e50d8cfe887e
hieuck <hieuck@users.noreply.github.com>
2023-04-27 06:42:24 +0700

    Update Cloudflare WARP.bat

--- Setup/Cloudflare WARP.bat
+++ Setup/Cloudflare WARP.bat
@@ -46,23 +46,26 @@ set "PROCESS=Cloudflare WARP.exe"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 ::Check Windows OS Version
-if "%OSVersion%"=="Yes" (
-	setlocal EnableDelayedExpansion
-	for /f "tokens=4 delims=[.] " %%i in ('ver') do (
-		set "version=%%i"
-	)
-
-	if !version! geq 6 (
-		echo Sorry, this software is not compatible with Windows 7. Exiting in 3 seconds...
-		for /l %%i in (3,-1,1) do (
-			echo Exiting in %%i seconds...
-			timeout /t 1 /nobreak >nul
-		)
-		exit
-	)
-	endlocal
+if /i "%OSVersion%"=="no" (
+    goto bypassCheckOSVersion
 )
 
+setlocal EnableDelayedExpansion
+for /f "tokens=4 delims=[.] " %%i in ('ver') do (
+	set "version=%%i"
+)
+
+if !version! geq 6 (
+	echo Sorry, this software is not compatible with Windows 7. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
+)
+endlocal
+
+:bypassCheckOSVersion
 :: Set code based on Windows Architecture
 :: Source link: 
 
