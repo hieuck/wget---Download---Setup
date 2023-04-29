cd90b0c527bc109de3b538e694fcd8ccbc073564
hieuck <hieuck@users.noreply.github.com>
2023-04-29 07:56:46 +0700

    Update Screenpresso.bat

--- Screenpresso.bat
+++ Screenpresso.bat
@@ -40,7 +40,7 @@ if exist "%SYSTEMROOT%\SysWOW64" (
 set "License="
 set "Extract7z="
 set "SOFTNAME=Screenpresso"
-set "PROCESS=ScreenpressoRpc.exe"&set "PROCESS=Screenpresso.exe"
+set "PROCESS=Screenpresso.exe"
 set "CheckOSVersion=No"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
