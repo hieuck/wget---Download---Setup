cf81d408cafe52d208aa71e3bd4c9c4f24d37bfd
hieuck <hieuck@users.noreply.github.com>
2023-05-03 10:47:29 +0700

    Update EVKey.bat

--- EVKey.bat
+++ EVKey.bat
@@ -45,7 +45,7 @@ set "CheckOSVersion=No"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 :: Set code based on Windows Architecture
-:: Source link: 
+:: Source link: https://github.com/lamquangminh/EVKey/releases/download/Release/EVKey.zip
 
 if %ARCH%==x86 (
 	set "PROCESS=EVKey32.exe"
