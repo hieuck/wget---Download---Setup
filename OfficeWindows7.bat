036af63ae68e36dc4222d6ba5b249e85f9ff3417
hieuck <hieuck@users.noreply.github.com>
2023-05-04 12:59:16 +0700

    Update OfficeWindows7.bat

--- OfficeWindows7.bat
+++ OfficeWindows7.bat
@@ -41,7 +41,7 @@ set "License=Yes"
 set "Extract7z="
 set "SOFTNAME=Office Windows 7"
 set "PROCESS=OfficeClickToRun.exe"
-set "CheckOSVersion=Yes"
+set "CheckOSVersion=No"
 set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 :: Set code based on Windows Architecture
