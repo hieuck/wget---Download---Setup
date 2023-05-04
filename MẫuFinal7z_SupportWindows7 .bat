eadf7b56b83101eaf1b2b796c980531f9fdc81df
hieuck <hieuck@users.noreply.github.com>
2023-05-04 21:38:39 +0700

    Create MẫuFinal7z_SupportWindows7 .bat

---
+++ MẫuFinal7z_SupportWindows7 .bat
@@ -0,0 +1,428 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::																								::
+::							https://linktr.ee/hieuckit											::
+::		Github:				https://github.com/hieuck/curl-uri-wget-download-setup				::
+::		Facebook:			https://www.facebook.com/ZzhieuhuhongzZ/							::
+::		Donate to me:		Vietcombank - 9966595263 - LE TRUNG HIEU							::
+::																								::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@ECHO OFF
+
+:: Run As Administrator
+>nul reg add hkcu\software\classes\.Admin\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\" &call \"%%2\" %%3" &set _= %*
+>nul fltmc || if "%f0%" neq "%~f0" ( cd.>"%tmp%\runas.Admin" &start "%~n0" /high "%tmp%\runas.Admin" "%~f0" "%_:"=""%" &exit /b )
+
+title _Hieuck.IT_'s Windows Application Setting Up...
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.
+@echo                 The current date and time are: %date% %time%
+@echo                 Dang Cau Hinh %SoftName%. Vui Long Cho...
+@echo off
+pushd "%~dp0"
+:: Detect Windows Architecture
+if exist "%SYSTEMROOT%\SysWOW64" (
+	set "ARCH=x64"
+) else (
+	set "ARCH=x86"
+)
+
+:: Set License Extract7z Soft Process Name SupportOldWindows Support32Bit User Agent
+set "License="
+set "Extract7z="
+set "SoftName=danvaoday"
+set "Process=danvaoday.exe"
+set "SupportOldWindows=Yes"
+set "Support32Bit=Yes"
+set "USERAGENT=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+:: Set code based on Windows Architecture
+:: Source link: 
+
+set "Link="
+set "LinkForOldWindows32bit="
+set "LinkForOldWindows64bit="
+
+
+set "LinkForNewWindows32bit="
+set "LinkForNewWindows64bit="
+
+set "SoftPath="
+set "SoftPathFor32bit="
+set "SoftPathFor64bit="
+set "QuietMode=/S"
+
+:: Set up information related to software cr4cking
+if "%License%"=="Yes" (
+	set "Admin=Yes"
+	set "Cr4ckFile=danvaoday"
+	set "Cr4ckPath=%SoftPath%"
+	set "Cr4ckLink=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/Cr4ck/!Cr4ckFile!.rar"
+	set "Link7zdll=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.dll"
+	set "Link7zexe=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.exe"
+)
+
+::Extract with 7z
+if "%Extract7z%"=="Yes" (
+	set "FileName=%SoftName%-HieuckIT.zip"
+	set "Admin=Yes"
+	set "Shortcut=Yes"
+	set "SoftPath=%PROGRAMFILES%\%SoftName%"
+	set "Link7zdll=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.dll"
+	set "Link7zexe=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.exe"
+) else (
+	set "Shortcut=No"
+	set "FileName=%SoftName%-HieuckIT.exe "
+)
+set "SOFTLOCATION=%SoftPath%\%Process%"
+
+:: Check Compatibility for 32-bit
+if /i "%Support32Bit%"=="no" (
+	echo Notice: This software is only compatible with Windows 64-bit operating systems. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
+)
+
+::Check Windows OS Version and Check Support Old Windows
+setlocal EnableDelayedExpansion
+for /f "tokens=4 delims=[.] " %%i in ('ver') do (
+	set "version1=%%i"
+)
+
+for /f "tokens=5 delims=[.] " %%i in ('ver') do (
+	set "version2=%%i"
+)
+set "version=%version1%.%version2%"
+
+if "%version%"=="6.1" goto ForWindows7
+goto ForWindows10
+endlocal
+
+:ForWindows7
+if /i "%SupportOldWindows%"=="no" (
+	echo Sorry, this software is not compatible with Windows 7. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
+) else (
+	if "%ARCH%"=="x86" (
+		if not "%LinkForOldWindows32bit%"=="" set "LINK=%LinkForOldWindows32bit%"
+		if not "%SoftPathFor32bit%"=="" set "SoftPath=%SoftPathFor32bit%"
+	) else (
+		if not "%LinkForOldWindows64bit%"=="" set "LINK=%LinkForOldWindows64bit%"
+		if not "%SoftPathFor64bit%"=="" set "SoftPath=%SoftPathFor64bit%"
+	)
+)
+goto NextStepForCheckOSVersion
+
+:ForWindows10
+if "%ARCH%"=="x86" (
+	if not "%LinkForNewWindows32bit%"=="" set "LINK=%LinkForNewWindows32bit%"
+	if not "%SoftPathFor32bit%"=="" set "SoftPath=%SoftPathFor32bit%"
+) else (
+	if not "%LinkForNewWindows64bit%"=="" set "LINK=%LinkForNewWindows64bit%"
+	if not "%SoftPathFor64bit%"=="" set "SoftPath=%SoftPathFor64bit%"
+)
+
+:NextStepForCheckOSVersion
+:: Check if Command Prompt is running with administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	echo Command Prompt is running as Administrator.
+) else (
+	if "%Admin%"=="Yes" (
+		echo Please Run as Administrator. Exiting in 3 seconds...
+		for /l %%i in (3,-1,1) do (
+			echo Exiting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+		exit
+	) else (
+		echo Warning: This program may not function correctly without administrator privileges.
+		for /l %%i in (3,-1,1) do (
+			echo Starting in %%i seconds...
+			timeout /t 1 /nobreak >nul
+		)
+	)
+)
+
+:: Terminate the %SoftName% Process
+tasklist | find /i "%Process%" > nul
+if %errorlevel% equ 0 (
+	taskkill /im "%Process%" /f
+)
+
+:: Save the value of the %time% variable before running the batch script
+set start_time=%time%
+
+:: Download
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application Downloading...
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.
+@echo                 The current date and time are: %date% %time%
+@echo                 Dang Tai %SoftName%. Vui Long Cho...
+@echo off
+pushd "%~dp0"
+echo Downloading %SoftName%...
+if exist "wget.exe" (
+	wget --no-check-certificate --show-progress -q -O "%FileName%" -U "%USERAGENT%" "%LINK%"
+) else (
+	curl -L --max-redirs 20 -A "%USERAGENT%" -o "%FileName%" "%LINK%" --insecure || (
+		if exist "%temp%\download_error.txt" del "%temp%\download_error.txt"
+		echo.
+		echo wget.exe or curl.exe not found to download, please download at: >> %temp%\download_error.txt
+		echo. >> %temp%\download_error.txt
+		echo wget: https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/wget.exe >> %temp%\download_error.txt
+		echo wget: https://eternallybored.org/misc/wget/ >> %temp%\download_error.txt
+		echo curl: https://curl.se/download.html >> %temp%\download_error.txt
+		type "%temp%\download_error.txt"
+		start "" "%temp%\download_error.txt"
+	)
+)
+
+if not exist "%FileName%" (
+	echo Download %SoftName% failed.
+	echo Please check your network connection. Exiting in 3 seconds...
+	for /l %%i in (3,-1,1) do (
+		echo Exiting in %%i seconds...
+		timeout /t 1 /nobreak >nul
+	)
+	exit
+)
+
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application Downloading 7-Zip...
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.
+@echo                 The current date and time are: %date% %time%
+@echo                 Dang Tai 7-Zip. Vui Long Cho...
+@echo off
+pushd "%~dp0"
+echo Downloading 7-Zip...
+if "%Extract7z%"=="Yes" (
+	if exist "wget.exe" (
+		wget --no-check-certificate --show-progress -q -O "7z.dll" -U "%USERAGENT%" "%Link7zdll%"
+		wget --no-check-certificate --show-progress -q -O "7z.exe" -U "%USERAGENT%" "%Link7zexe%"
+	) else (
+		curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.dll" "%Link7zdll%" --insecure
+		curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.exe" "%Link7zexe%" --insecure
+	)
+)
+
+:: Install
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application Installing...
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.
+@echo                 The current date and time are: %date% %time%
+@echo                 Dang Cai Dat %SoftName%. Vui Long Cho...
+@echo off
+pushd "%~dp0"
+echo Installing %SoftName%...
+if "%Extract7z%"=="Yes" (
+	@7z.exe x "%FileName%" -o"%SoftPath%" -aoa -y
+) else (
+	"%FileName%" %QUIETMODE%
+)
+
+:: Check Installation Process
+echo Checking if %SoftName% installation is complete...
+setlocal EnableDelayedExpansion
+set count=0
+:waitloop
+timeout /t 1 /nobreak > nul
+set /a count+=1
+if exist "%SOFTLOCATION%" goto installed
+if !count! equ 30 goto timeout
+goto waitloop
+:timeout
+echo Timeout: %SoftName% installation has not completed in 30 seconds.
+goto end
+:installed
+echo %SoftName% has been installed successfully.
+timeout /t 3
+:end
+
+:: License
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application Cr4cking...
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.
+@echo                 The current date and time are: %date% %time%
+@echo                 Dang Cau Hinh %SoftName%. Vui Long Cho...
+@echo off
+pushd "%~dp0"
+if "%License%"=="Yes" (
+	echo Cr4cking %SoftName%...
+	if exist "wget.exe" (
+		if not exist "7z.dll" if not exist "7z.exe" (
+			wget --no-check-certificate --show-progress -q -O "7z.dll" -U "%USERAGENT%" "%Link7zdll%"
+			wget --no-check-certificate --show-progress -q -O "7z.exe" -U "%USERAGENT%" "%Link7zexe%"
+		)
+		wget --no-check-certificate --show-progress -q -O "%CR4CKFILE%" -U "%USERAGENT%" "%CR4CKLINK%"
+	) else (
+		if not exist "7z.dll" if not exist "7z.exe" (
+			curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.dll" "%Link7zdll%" --insecure
+			curl -L --max-redirs 20 -A "%USERAGENT%" -o "7z.exe" "%Link7zexe%" --insecure
+		)
+		curl -L --max-redirs 20 -A "%USERAGENT%" -o "%CR4CKFILE%" "%CR4CKLINK%" --insecure
+	)
+	if exist "%CR4CKFILE%" (
+		@7z.exe x -p123 "%CR4CKFILE%" -o"%CR4CKPATH%" -aoa -y
+		echo Successfully Cr4cked %SoftName%.
+		del "%CR4CKFILE%"
+	) else (
+		echo Cr4cking %SoftName% failed.
+		echo Please try running the script as Administrator.
+		goto CleanUp
+	)
+)
+
+:: Shortcut
+if /i "%Shortcut%"=="no" (
+    echo Creating Shortcut is skipped.
+    goto CleanUp
+)
+
+if exist "%SOFTLOCATION%" (
+	set "TargetFile=%SOFTLOCATION%"
+) else (
+	echo %SoftName% does not exist in directory "%SoftPath%". Exiting script.
+	exit /b 1
+)
+
+set "ShortcutName=%SoftName%.lnk"
+set "ShortcutPath=%PUBLIC%\Desktop\%ShortcutName%"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%ShortcutPath%" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%TargetFile%" >> CreateShortcut.vbs
+echo oLink.WorkingDirectory = "%SoftPath%" >> CreateShortcut.vbs
+echo oLink.Description = "Shortcut to %SoftName%" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
+
+if exist "%PUBLIC%\Desktop\%ShortcutName%" (
+	echo Creating Shortcut complete.
+) else (
+	echo Creating Shortcut failed.
+)
+
+:: Clean Up
+:CleanUp
+@ECHO OFF
+title _Hieuck.IT_'s Windows Application Cleaning Up...
+color 0B
+mode con:cols=100 lines=17
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл лллллл ллл   ллл  ллллл  ллл  лл ллл ллллллллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo        Вл       ллллллллл ллл лллллл ллл   ллл ллл     ллллл   ллл    ллл
+@echo       л         ллл   ллл ллл ллл    ллл   ллл ллл   л ллл лл  ллл    ллл
+@echo     Бл   ВВВВВ  ллл   ллл ллл лллллл ллллллллл  ллллл  ллл  лл ллл    ллл В
+@echo.
+@echo                 The current date and time are: %date% %time%
+@echo                 Dang Don Dep %SoftName%. Vui Long Cho...
+@echo off
+pushd "%~dp0"
+if exist "%FileName%" del "%FileName%"
+if exist "%temp%\download_error.txt" del "%temp%\download_error.txt"
+if exist "7z.dll" del "7z.dll"
+if exist "7z.exe" del "7z.exe"
+
+:: Save the value of the %time% variable after the batch script finishes
+set end_time=%time%
+
+:: Convert the start and end times to seconds
+for /f "tokens=1-3 delims=:." %%a in ("%start_time%") do set /a "start_seconds=(((%%a*60)+1%%b %% 100)*60)+1%%c %% 100"
+for /f "tokens=1-3 delims=:." %%a in ("%end_time%") do set /a "end_seconds=(((%%a*60)+1%%b %% 100)*60)+1%%c %% 100"
+
+:: Calculate the elapsed time in seconds
+set /a elapsed_time=%end_seconds%-%start_seconds%
+
+echo Time elapsed: %elapsed_time% seconds.
+
+echo The script will automatically close in 3 seconds.
+for /l %%i in (3,-1,1) do (
+	echo Closing in %%i seconds...
+	timeout /t 1 /nobreak >nul
+	if exist "7z.dll" (
+		tasklist | find /i "7z.dll" > nul
+		if %errorlevel% equ 0 taskkill /im "7z.dll" /f
+		del "7z.dll"
+	)
+
+	if exist "7z.exe" (
+		tasklist | find /i "7z.exe" > nul
+		if %errorlevel% equ 0 taskkill /im "7z.exe" /f
+		del "7z.exe"
+	)
+
+	if exist "%FileName%" (
+		tasklist | find /i "%FileName%" > nul
+		if %errorlevel% equ 0 taskkill /im "%FileName%" /f
+		del "%FileName%"
+	)
+)
+echo Please close the script manually if automatically close fails.
+popd
