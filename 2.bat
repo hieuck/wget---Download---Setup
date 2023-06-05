49c3241be33c575d8270439825f00723c1aa3bd6
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2023-06-05 08:46:37 +0700

    Create 2.bat

---
+++ 2.bat
@@ -0,0 +1,44 @@
+@echo off
+set "SoftName=DirectX"
+set "FileName=exe"
+
+setlocal enabledelayedexpansion
+for %%F in ("%FileName%") do (
+	set "BaseName=%%~nF"
+	if not "%%~xF"=="" (
+		set "Extension=%%~xF"
+	) else (
+		set "Extension=.HieuckIT"
+		set "BaseName=!FileName!"
+	)
+)
+
+echo BaseName: %BaseName%
+echo Extension: %Extension%
+echo.
+if not "%FileName%"=="" (
+	if not "%BaseName%"=="" (
+		if /i "%FileName%"=="msi" (
+			set "BaseName=%SoftName%"
+			set "Extension=.msi"
+			set "FileName=!BaseName!!Extension!"
+		) else if /i "%FileName%"=="exe" (
+			set "BaseName=%SoftName%"
+			set "Extension=.exe"
+			set "FileName=!BaseName!!Extension!"
+		) else (
+			set "FileName=%BaseName%%Extension%"
+		)
+	) else (
+		set "FileName=%SoftName%%Extension%"
+	)
+) else (
+	set "FileName=%SoftName%.HieuckIT"
+)
+
+
+echo BaseName: %BaseName%
+echo Extension: %Extension%
+echo FileName: %FileName%
+pause
+endlocal
