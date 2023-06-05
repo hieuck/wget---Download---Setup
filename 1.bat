d76f6ac99a68db7cdcb52dfadf118554288b6ce1
Lê Trung Hiếu <30469654+hieuck@users.noreply.github.com>
2023-06-05 08:26:37 +0700

    Test 1.bat

--- 1.bat
+++ 1.bat
@@ -26,3 +26,96 @@ if not "%FileName%"=="" (
 echo FileName: %FileName%
 pause
 endlocal
+
+@echo off
+set "SoftName=DirectX"
+set "FileName=."  REM Đặt giá trị FileName tại đây
+
+setlocal enabledelayedexpansion
+for %%F in ("%FileName%") do (
+    set "BaseName=%%~nF"
+    if not "%%~xF"=="" (
+        set "Extension=%%~xF"
+    ) else (
+        set "Extension=.HieuckIT"
+        set "BaseName=!FileName!"
+    )
+)
+
+REM Xác định giá trị của biến "Extension" dựa trên phần mở rộng trong "FileName"
+set "Extension=%Extension:~1%"
+if "%Extension%"=="" (
+    if "%FileName%"=="msi" (
+        set "Extension=.msi"
+    ) else if "%FileName%"=="exe" (
+        set "Extension=.exe"
+    ) else if "%FileName%"=="zip" (
+        set "Extension=.zip"
+    ) else (
+        set "Extension=.HieuckIT"
+    )
+)
+REM Kết thúc xác định giá trị của biến "Extension"
+
+echo BaseName: %BaseName%
+echo Extension: %Extension%
+
+if not "%FileName%"=="" (
+    if not "%BaseName%"=="" (
+        set "FileName=%BaseName%%Extension%"
+    ) else (
+        set "FileName=%SoftName%%Extension%"
+    )
+) else (
+    set "FileName=%SoftName%.HieuckIT"
+)
+echo FileName: %FileName%
+pause
+endlocal
+
+
+@echo off
+set "SoftName=DirectX"
+set "FileName=."  REM Đặt giá trị FileName tại đây
+
+setlocal enabledelayedexpansion
+for %%F in ("%FileName%") do (
+    set "BaseName=%%~nF"
+    if not "%%~xF"=="" (
+        set "Extension=%%~xF"
+    ) else (
+        set "Extension=.HieuckIT"
+        set "BaseName=!FileName!"
+    )
+)
+
+REM Xác định giá trị của biến "Extension" dựa trên phần mở rộng trong "FileName"
+set "Extension=%Extension:~1%"
+if "%Extension%"=="" (
+    if "%FileName%"=="msi" (
+        set "Extension=.msi"
+    ) else if "%FileName%"=="exe" (
+        set "Extension=.exe"
+    ) else if "%FileName%"=="zip" (
+        set "Extension=.zip"
+    ) else (
+        set "Extension=.HieuckIT"
+    )
+)
+REM Kết thúc xác định giá trị của biến "Extension"
+
+echo BaseName: %BaseName%
+echo Extension: %Extension%
+
+if not "%FileName%"=="" (
+    if not "%BaseName%"=="" (
+        set "FileName=%BaseName%%Extension%"
+    ) else (
+        set "FileName=%SoftName%%Extension%"
+    )
+) else (
+    set "FileName=%SoftName%.HieuckIT"
+)
+echo FileName: %FileName%
+pause
+endlocal
