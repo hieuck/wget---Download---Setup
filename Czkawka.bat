59e7e15138c3dacd9b78b46950f62ed8ea626915
hieuck <hieuck@users.noreply.github.com>
2023-07-23 17:04:33 +0700

    update new code

--- Czkawka.bat
+++ Czkawka.bat
@@ -405,6 +405,7 @@ if not exist "%FileNameDLwB%" (
 	timeout /t 1 /nobreak >nul & goto CheckExist
 )
 
+echo Download completed with the browser. Installation in progress...
 move /y "%FileNameDLwB%" "%~dp0%FileName%"
 move /y "%FileNameDLwBFFmpeg%" "%~dp0%NameFFmpeg%.zip"
 
