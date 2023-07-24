a30d8555e72a5abf889493395ebd16a8f00d2b87
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2023-07-24 19:01:37 +0700

    Update 1Click-HieuckIT.bat

--- 1Click-HieuckIT.bat
+++ 1Click-HieuckIT.bat
@@ -32,7 +32,7 @@ echo.
 @echo off
 REM Required Configuration Settings
 
-set "SoftName=1-Click"
+set "SoftName=1-Click-HieuckIT"
 
 set "UserAgent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
