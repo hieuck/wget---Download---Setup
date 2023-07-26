1fecb3f5bb31a7df9fb567f7f97b31d2251b7984
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2023-07-26 18:03:03 +0700

    Update BatteryMon.bat

--- BatteryMon.bat
+++ BatteryMon.bat
@@ -48,7 +48,7 @@ set "Support32Bit=Yes"
 set "UserAgent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 REM Set code based on Windows Architecture
-REM Source Link: 
+REM Source Link: https://www.passmark.com/products/batmon/
 
 set "LinkForOldWindows="
 set "LinkForOldWindows32bit="
