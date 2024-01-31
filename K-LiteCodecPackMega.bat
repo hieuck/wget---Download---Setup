096aca594760a66d9ce324411c3746c573592916
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2024-01-31 15:26:26 +0700

    update new version

--- K-LiteCodecPackMega.bat
+++ K-LiteCodecPackMega.bat
@@ -44,7 +44,7 @@ set "Process=mpc-hc.exe"
 if exist "%SystemRoot%\SysWOW64" set "Process=mpc-hc64.exe"
 
 set "FileName="
-set "SoftNameVersion=1800"
+set "SoftNameVersion=1805"
 set "FileDLwB=K-Lite_Codec_Pack*.exe"
 
 set "OpenAfterInstall="
