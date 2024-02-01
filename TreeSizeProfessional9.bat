98a06ef7b98c7831608db79827d0f6d92d1f13fd
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2024-02-01 17:15:27 +0700

    Update to  9.1.1.1869

--- TreeSizeProfessional9.bat
+++ TreeSizeProfessional9.bat
@@ -43,7 +43,7 @@ set "SoftName=TreeSize Professional 9"
 set "Process=TreeSize.exe"
 
 set "FileName="
-set "SoftNameVersion=9.0.3.1852"
+set "SoftNameVersion=9.1.1.1869"
 set "FileDLwB=TreeSize*exe"
 
 set "OpenAfterInstall="
