9f5fe0f269d1cfeeaf90ecf161c6c523d702745a
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2024-06-22 07:26:06 +0700

    Update CloudflareWARP.bat

--- CloudflareWARP.bat
+++ CloudflareWARP.bat
@@ -44,7 +44,9 @@ set "Process=Cloudflare WARP.exe"
 
 set "FileName="
 set "SoftNameVersion="
-set "FileDLwB=Cloudflare_WARP*msi"
+set "FileDLwB=Cloudflare_WARP*"
+
+set "OpenAfterInstall="
 
 set "SupportOldWindows=No"
 set "Support32Bit=No"
@@ -61,8 +63,32 @@ for /f "tokens=1-4 delims=." %%a in ("%SoftNameVersion%") do (
 REM Set code based on Windows Architecture
 REM Source Link: https://1.1.1.1/
 
+set "LinkForOldWindows="
+set "LinkForOldWindows32bit="
+set "LinkForOldWindows64bit="
+
+set "Link=https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi"
+set "LinkForAllWindows32bit="
+set "LinkForAllWindows64bit="
+
+set "LinkFromGithub="
+set "LinkFromDropbox="
+set "LinkFromOneDrive="
+
+set "SoftPath=%ProgramFiles%\Cloudflare\Cloudflare WARP"
+set "SoftPathFor32bit="
+set "SoftPathFor64bit="
+
+set "QuietMode=/quiet /qn /norestart"
+
+set "Cr4ckFile="
+set "Cr4ckPath="
+
+set "Shortcut="
+set "NoticeOption="
+
 REM MenuChoice Configuration
-setlocal
+setlocal EnableDelayedExpansion
 
 :menu
 set "Menu1=Official Website"
@@ -70,14 +96,41 @@ set "Menu2=My Github"
 set "Menu3=My Dropbox"
 set "Menu4=My OneDrive"
 
-echo Do you want to use the download link from:
-echo 1. %Menu1%				2. %Menu2%
-echo.
-echo 3. %Menu3%					4. %Menu4%
+REM Use the pattern "2. %Menu2% & echo." !MenuOptions!3. %Menu3%" to break the line.
+set "MenuOptions="
+if not "!LinkFromGithub!"=="" (
+	set "MenuOptions=!MenuOptions!	2. %Menu2%"
+)
+if not "!LinkFromDropbox!"=="" (
+	set "MenuOptions=!MenuOptions!	3. %Menu3%"
+)
+if not "!LinkFromOneDrive!"=="" (
+	set "MenuOptions=!MenuOptions!	4. %Menu4%"
+)
+
+if not "!MenuOptions!"=="" (
+	echo Do you want to use the download link from:
+	echo.
+	echo 1. %Menu1%%MenuOptions%
+) else (
+	echo You have chosen to download from: %Menu1%
+	goto NextStepAfterChosen
+)
 
 REM The number corresponding to the default choice
 set "defaultChoice=1"
-echo Select an option (1 or 2 or 3 or 4) [Default is %defaultChoice%]: 
+
+set "OptionsChoice="
+if not "!LinkFromGithub!"=="" (
+	set "OptionsChoice=!OptionsChoice! or 2"
+)
+if not "!LinkFromDropbox!"=="" (
+	set "OptionsChoice=!OptionsChoice! or 3"
+)
+if not "!LinkFromOneDrive!"=="" (
+	set "OptionsChoice=!OptionsChoice! or 4"
+)
+echo Select an option (1%OptionsChoice%) [Default is %defaultChoice%]: 
 choice /c 1234 /t 5 /d %defaultChoice% /n >nul
 
 REM Check the errorlevel to determine the choice made by the user
@@ -95,23 +148,52 @@ REM Display the choice made
 if "%choice%"=="1" (
 	REM Official Website
 	echo You have chosen to download from: %Menu1%
-	set "Link=https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi"
 	goto NextStepAfterChosen
 ) else if "%choice%"=="2" (
 	REM My Github
 	echo You have chosen to download from: %Menu2%
-	set "Link="
-	goto NextStepAfterChosen
+	if not "%LinkFromGithub%"=="" (
+		set "LinkForOldWindows="
+		set "LinkForOldWindows32bit="
+		set "LinkForOldWindows64bit="
+
+		set "Link=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/Setup/%LinkFromGithub%"
+		set "LinkForAllWindows32bit="
+		set "LinkForAllWindows64bit="
+		goto NextStepAfterChosen
+	) else (
+		echo No download link available yet in %Menu2%.&echo.&goto menu
+	)
 ) else if "%choice%"=="3" (
 	REM My Dropbox
 	echo You have chosen to download from: %Menu3%
-	set "Link="
-	goto NextStepAfterChosen
+	if not "%LinkFromDropbox%"=="" (
+		set "LinkForOldWindows="
+		set "LinkForOldWindows32bit="
+		set "LinkForOldWindows64bit="
+
+		set "Link=%LinkFromDropbox%"
+		set "LinkForAllWindows32bit="
+		set "LinkForAllWindows64bit="
+		goto NextStepAfterChosen
+	) else (
+		echo No download link available yet in %Menu3%.&echo.&goto menu
+	)
 ) else if "%choice%"=="4" (
 	REM My OneDrive
 	echo You have chosen to download from: %Menu4%
-	set "Link="
-	goto NextStepAfterChosen
+	if not "%LinkFromOneDrive%"=="" (
+		set "LinkForOldWindows="
+		set "LinkForOldWindows32bit="
+		set "LinkForOldWindows64bit="
+
+		set "Link=%LinkFromOneDrive%"
+		set "LinkForAllWindows32bit="
+		set "LinkForAllWindows64bit="
+		goto NextStepAfterChosen
+	) else (
+		echo No download link available yet in %Menu4%.&echo.&goto menu
+	)
 ) else (
 	echo Invalid choice. Please select 1, 2, 3, or 4.
 	goto menu
@@ -119,26 +201,6 @@ if "%choice%"=="1" (
 
 endlocal
 :NextStepAfterChosen
-
-set "LinkForOldWindows="
-set "LinkForOldWindows32bit="
-set "LinkForOldWindows64bit="
-
-set "Link=%Link%"
-set "LinkForAllWindows32bit="
-set "LinkForAllWindows64bit="
-
-set "SoftPath=%ProgramFiles%\Cloudflare\Cloudflare WARP"
-set "SoftPathFor32bit="
-set "SoftPathFor64bit="
-
-set "QuietMode=/quiet /qn /norestart"
-
-set "Cr4ckFile="
-set "Cr4ckPath="
-
-set "Shortcut="
-set "NoticeOption="
 
 REM Convert to direct download Link.
 setlocal enabledelayedexpansion
@@ -297,7 +359,12 @@ if /i "%Extract7z%"=="Yes" (
 REM Set up information related to software cr4cking
 if /i "%License%"=="Yes" (
 	set "Admin=Yes"
-	set "Cr4ckLink=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/Cr4ck/!Cr4ckFile!.rar"
+	:: Extracting the necessary parts from Cr4ckFile
+	for /f "tokens=1,3 delims=-" %%a in ("%Cr4ckFile%") do (
+		set "prefix=%%a"
+		set "suffix=%%b"
+	)
+	set "Cr4ckLink=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/Cr4ck/!prefix!!suffix!/!Cr4ckFile!.rar"
 	set "Link7zdll=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.dll"
 	set "Link7zexe=https://github.com/hieuck/curl-uri-wget-download-setup/raw/main/7z/7z.exe"
 	if /i "%ARCH%"=="x86" (
